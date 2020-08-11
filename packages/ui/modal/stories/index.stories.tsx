768209d0307f553e3ecab54d51a70dba98230239
liushaozhen <804199994@qq.com>
2020-08-11 19:06:23 +0800

    feat(modal): 新增modal组件

--- packages/ui/modal/stories/index.stories.tsx
+++ packages/ui/modal/stories/index.stories.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button } from 'antd'
+import { Button } from 'antd';
 import Modal from '../src';
 
 export default {
@@ -12,19 +12,21 @@ export const Demo = () => {
 
     return (
         <div style={{ padding: 30 }}>
-            <Button type="primary" onClick={() => { setVisible(true) }}>
+            <Button type="primary" onClick={() => setVisible(true)}>
                 打开基础modal
             </Button>
             <Modal
                 title="我是标题我是标题"
                 visible={visible}
                 onOk={() => setVisible(false)}
-                onCancel={() => { setVisible(false) }}
+                onCancel={() => setVisible(false)}
             >
-                何时使用：需要用户处理事务，又不希望跳转页面以致打断工作流程时，可以使用 Modal 在当前页面正中打开一个浮层，承载相应的操作。何时使用：需要用户处理事务，又不希望跳转页面以致打断工作流程时，可以使用 Modal 在当前页面正中打开一个浮层，承载相应的操作。何时使用，需要用户处理事务，又不希望跳转页面以致打断工作流程时，可以使用 Modal
+                何时使用：需要用户处理事务，又不希望跳转页面以致打断工作流程时，可以使用 Modal 在当前页面正中打开一个浮层，承载相应的操作。
+                何时使用：需要用户处理事务，又不希望跳转页面以致打断工作流程时，可以使用 Modal 在当前页面正中打开一个浮层，承载相应的操作。
+                何时使用：需要用户处理事务，又不希望跳转页面以致打断工作流程时，可以使用 Modal 在当前页面正中打开一个浮层，承载相应的操作。
             </Modal>
             <p></p>
-            <Button type="primary" onClick={() => { setBigVisible(true) }}>
+            <Button type="primary" onClick={() => setBigVisible(true)}>
                 打开大数据modal
             </Button>
             <Modal
@@ -32,7 +34,7 @@ export const Demo = () => {
                 title="我是标题我是标题"
                 visible={visibleBig}
                 onOk={() => setBigVisible(false)}
-                onCancel={() => { setBigVisible(false) }}
+                onCancel={() => setBigVisible(false)}
             >
                 何时使用：需要用户处理事务，又不希望跳转页面以致打断工作流程时，可以使用 Modal 在当前页面正中打开一个浮层，承载相应的操作。
                 何时使用：需要用户处理事务，又不希望跳转页面以致打断工作流程时，可以使用 Modal 在当前页面正中打开一个浮层，承载相应的操作。
