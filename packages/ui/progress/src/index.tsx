d183b830490b86e8a64ca1e05f4b599a833e1f9e
liushaozhen <804199994@qq.com>
2020-08-11 13:39:41 +0800

    fix(progress): progress lint fix

--- packages/ui/progress/src/index.tsx
+++ packages/ui/progress/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { Progress as AntdProgress} from 'antd';
-import { ProgressProps as AntdProgressProps} from 'antd/lib/Progress';
+import { Progress as AntdProgress } from 'antd';
+import { ProgressProps as AntdProgressProps } from 'antd/lib/Progress';
 import classNames from 'classnames';
 import './index.less';
 
@@ -9,7 +9,7 @@ const clsPrefix = 'osui-progress';
 export type ProgressProps = AntdProgressProps;
 
 const Progress: React.FC<ProgressProps> = props => {
-    return <AntdProgress {...props}  className={classNames(clsPrefix, props.className)} />;
+    return <AntdProgress {...props} className={classNames(clsPrefix, props.className)} />;
 };
 
 export default Progress;
