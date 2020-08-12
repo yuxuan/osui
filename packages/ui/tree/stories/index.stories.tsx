01a0e7432629afd64e9c10422a9bdf3f9c23dadd
yuxuanhuo <yuxuan.huo2011@gmail.com>
2020-08-12 16:00:27 +0800

    wip tree

---
+++ packages/ui/tree/stories/index.stories.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {IconNaviDoc} from '@osui/icons';
+
+export default {
+    title: 'OSUI-tree',
+};
+
+export const Demo = () => {
+    return (<IconNaviDoc />);
+};
