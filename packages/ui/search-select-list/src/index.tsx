dfb8652356a64fd13c17559d040cd6b35ae773bc
zs <zhusen@oschina.com>
2020-08-13 11:40:19 +0800

    feat(select-search-list): 新增select-search-list组件

--- packages/ui/search-select-list/src/index.tsx
+++ packages/ui/search-select-list/src/index.tsx
@@ -1,50 +1,128 @@
 import * as React from 'react';
 import {Popover, Button, Input, Menu} from 'antd';
+import {PopoverProps as AntdPooverProps} from 'antd/lib/popover';
+import classNames from 'classnames';
 import {SearchOutlined} from '@ant-design/icons';
 import './index.less';
 
-// const clsPrefix = 'osui-select-search-list';
+const clsPrePopfix = 'osui-search-select-list-popover';
+const clsPreBtnfix = 'osui-search-select-list-button';
 
-
-export interface SeacrhSelectListProps {
-    menuList?: any[];
-    list?: any[];
+/**
+ * selectList的每个选项
+ * todo, icon应该定义成什么类型，之后和btn样式一起修改，btn也需要icon
+ */
+export interface LiItemProps{
+    label: string;
+    value: string;
+    icon?: any;
+    handleClickItem: (item: any) => void;
 }
+const LiItem: React.FC<LiItemProps> = props => {
+    function handleClickItem() {
+        props.handleClickItem(props);
+    }
+    return (<li
+        onClick={handleClickItem}
+        key={props.value}
+        value={props.value}
+    >
+        {/* {icon? icon : null}*/}
+        <span>{props.label}</span>
+    </li>
+    );
+};
 
-
+/**
+* popoverClassName:popover的className
+* btnClassName:button的className
+* placeholder:input搜索的placeholder内容
+* btnName:按钮的名称
+* menuList:导航列表，若不传的话就没有上面的导航列表
+* selectlist:下拉选项列表
+* defaultMenuSelect:默认选中的menu
+* handleClickItem:点击menu回传的函数
+* handleSearch:点击搜索回传的函数
+* handleClickMenu:点击下拉选项回传的函数
+*/
+export interface SeacrhSelectListProps extends AntdPooverProps{
+    popoverClassName?: string;
+    btnClassName?: string;
+    placeholder?: string;
+    btnName?: string;
+    menuList?: any[];
+    selectlist?: any[];
+    defaultMenuSelect?: string;
+    handleClickItem: (item: any) => void;
+    handleSearch: (val: any) => void;
+    handleClickMenu?: (key: any) => void;
+}
 const SeacrhSelectList: React.FC<SeacrhSelectListProps> = props => {
-    const {menuList, list} = props;
-    // eslint-disable-next-line
-    console.log(menuList, list);
+    const {
+        popoverClassName,
+        btnClassName,
+        btnName,
+        menuList,
+        selectlist,
+        defaultMenuSelect,
+        placeholder,
+        handleClickItem,
+        handleSearch,
+        handleClickMenu,
+    } = props;
+    let menu = null;
 
-    const menu = menuList && (<Menu mode="horizontal">
-        {
-            menuList.map((item: any) => <Menu.Item key={item.index}>{item} </Menu.Item>)
-        }
-    </Menu>);
-    const content = (<div className="select-list-wrap">
-        <Input
-            style={{width: 200}}
-            placeholder="请输入搜索关键字"
-            className="input"
-            suffix={
-                <SearchOutlined />
+    const [searchVal, setSearchVal] = React.useState();
+    function handleChangeSearch(e: any) {
+        setSearchVal(e.target.value);
+    }
+    function handleSearchFunc() {
+        handleSearch(searchVal);
+    }
+    // menuList 存在，则赋默认menu值，若没有则第一个为默认
+    if (menuList && menuList.length) {
+        const selectSelectedKey = defaultMenuSelect ? [`${defaultMenuSelect}`] : [menuList[0].value];
+        menu = (<Menu
+            mode="horizontal"
+            defaultSelectedKeys={selectSelectedKey}
+            onClick={handleClickMenu}
+        >
+            {
+                menuList.map((item: any) => <Menu.Item key={item.value}>{item.label}</Menu.Item>)
             }
-        />
-        <ul className="list">
-            {list && list.map(item => <li key={item.value} value={item.value}>{item.label}</li>)}
+        </Menu>);
+    }
+    const content = (<div className="select-list-wrap">
+        <div className="input-box">
+            <Input
+                placeholder={placeholder || '请输入搜索的内容'}
+                value={searchVal}
+                onChange={handleChangeSearch}
+                suffix={
+                    <SearchOutlined onClick={handleSearchFunc} />
+                }
+            />
+        </div>
+        <ul className="list-box">
+            {selectlist && selectlist.length && selectlist.map(item => (<LiItem
+                handleClickItem={handleClickItem}
+                label={item.label}
+                value={item.value}
+                key={item.value}
+            />
+            ))}
         </ul>
     </div>);
+    {/* todo button OsuiSelect完成后，看基于select修改还是自己写btn */}
     return (
         <>
             <Popover
-                placement="topLeft"
-                overlayClassName={'osui-select-search-list-popover'}
+                overlayClassName={classNames(clsPrePopfix, popoverClassName)}
                 content={content}
                 title={menu}
-                visible
+                {...props}
             >
-                <Button type="primary">Hover me</Button>
+                <Button className={classNames(clsPreBtnfix, btnClassName)} type="primary">{btnName}</Button>
             </Popover>
         </>
     );
--- packages/ui/search-select-list/stories/index.stories.tsx
+++ packages/ui/search-select-list/stories/index.stories.tsx
@@ -1,25 +1,70 @@
-import * as React from 'react';
+import React, {useState} from 'react';
+import {Row} from 'antd';
 import SearchSelectList from '../src';
 
 export default {
-    title: 'OSUI-Search-select-list',
+    title: 'OSUI-Search-Select-List',
 };
 
 export const Demo = () => {
-    const menuList = ['分支', '标签'];
-    const list = [{
-        value: 1,
-        label: '选项一',
+    const menuList = [{
+        value: 'key1',
+        label: '分支',
     }, {
-        value: 1,
-        label: '选项二',
-    }, {
-        value: 1,
-        label: '选项三',
+        value: 'key2',
+        label: '标签',
     }];
+    const selectlist: any[] = [];
+    for (let i = 0; i < 10; i++) {
+        selectlist.push({
+            value: i,
+            label: `选项${i + 1}`,
+            icon: '',
+        });
+    }
+    const [selectVal, setSelectVal] = useState();
+    const [searchVal, setSearchVal] = useState();
+    const [menuVal, setMenuVal] = useState('');
+    function handleClickItem(item: any): void {
+        setSelectVal(item.label);
+    }
+    function handleSearch(val: any): void {
+        setSearchVal(val);
+    }
+    function handleClickMenu(e: any): void {
+        const val = menuList.filter(item => item.value === e.key)[0].label;
+        setMenuVal(val);
+    }
     return (
         <>
-            <SearchSelectList menuList={menuList} list={list} />
+            <p>当前点击的menu是 <span style={{color: '#4C88FF'}}>{menuVal}</span></p>
+            <p>当前搜索的是 <span style={{color: '#4C88FF'}}>{searchVal}</span></p>
+            <p>当前点击了 <span style={{color: '#4C88FF'}}>{selectVal}</span></p>
+            <Row style={{marginBottom: 30}}>
+                <SearchSelectList
+                    btnName={'存在menu'}
+                    defaultMenuSelect={'key1'}
+                    placement="topLeft"
+                    menuList={menuList}
+                    selectlist={selectlist}
+                    handleClickItem={handleClickItem}
+                    handleSearch={handleSearch}
+                    handleClickMenu={handleClickMenu}
+                />
+            </Row>
+            <Row>
+                <SearchSelectList
+                    popoverClassName={'myPopoverClassName'}
+                    btnClassName={'myBtnClassName'}
+                    btnName={'不存在menu'}
+                    defaultMenuSelect={'key1'}
+                    placeholder={'请输入'}
+                    placement="topLeft"
+                    selectlist={selectlist}
+                    handleClickItem={handleClickItem}
+                    handleSearch={handleSearch}
+                />
+            </Row>
         </>
     );
 };
