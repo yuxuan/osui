2b0078d3a34d23c381ba2cf762562173445fa5f5
YangPengPc <1324901147@qq.com>
2020-08-13 17:24:20 +0800

    feat:update tag

--- packages/ui/tag/src/index.tsx
+++ packages/ui/tag/src/index.tsx
@@ -1,17 +1,26 @@
 import * as React from 'react';
 import {Tag as AntdTag} from 'antd';
-import {TagProps as AntdTagProps} from 'antd/lib/Tag';
+import {TagProps as AntdTagProps, CheckableTagProps as AntdCheckableTagProps} from 'antd/lib/Tag';
 import classNames from 'classnames';
 import './index.less';
 
 const clsPrefix = 'osui-tag';
 
 export type TagProps = AntdTagProps;
+export type CheckableTagProps = AntdCheckableTagProps;
 
-const Tag: React.FC<TagProps> = props => {
-    return <AntdTag className={classNames(clsPrefix, props.className)} {...props} />;
+interface TagInterface extends React.FC<TagProps> {
+    CheckableTag: typeof AntdTag.CheckableTag;
+}
+
+const Tag: TagInterface = props => {
+    return <AntdTag {...props} className={classNames(clsPrefix, props.className)} />;
 };
 
-Tag.CheckableTag = AntdTag.CheckableTag;
+const CheckableTag: React.FC<CheckableTagProps> = props => {
+    return <AntdTag.CheckableTag className={classNames(`${clsPrefix}-checkable`, props.className)} {...props} />;
+};
+
+Tag.CheckableTag = CheckableTag;
 
 export default Tag;
