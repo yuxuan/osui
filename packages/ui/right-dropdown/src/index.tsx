7d63e5a31123f7800b5e4e1375bbaa06d201d10a
YangPengPc <1324901147@qq.com>
2020-08-14 18:29:39 +0800

    feat:add MenuDropdown

--- packages/ui/right-dropdown/src/index.tsx
+++ packages/ui/right-dropdown/src/index.tsx
@@ -1,5 +1,5 @@
 /**
- * @file Right-dropdown 右键菜单
+ * @file Menu-dropdown 右键菜单
  * @author yangpeng
  * */
 
@@ -9,61 +9,32 @@ import {DropDownProps as AntdDropdownProps} from 'antd/lib/Dropdown';
 import classNames from 'classnames';
 import './index.less';
 
-const clsPrefix = 'osui-right-dropdown';
+const clsPrefix = 'osui-menu-dropdown';
 
 export type DropdownProps = AntdDropdownProps;
 
-const RightDropdown: React.FC<DropdownProps> = props => {
-    const { SubMenu } = Menu;
+interface MenuDropdownProps extends React.FC<DropdownProps> {
+    menuDate?: any[];
+    className?: string;
+    handleMenuClick?: any;
+}
 
-    const menuDate = [
-        {
-            title: '新建 Pull Request',
-            value: 'newPr',
-        },
-        {
-            title: '新建分支',
-            value: 'newBranch',
-        },
-        {
-            title: '新建组件',
-            value: 'newComponents',
-        },
-        {
-            title: '新建项目',
-            child: [
-                {
-                    title: '计划 Project',
-                    value: 'project',
-                },
-                {
-                    title: '文档 DOC',
-                    value: 'doc',
-                },
-                {
-                    title: '流水线 Pipe',
-                    value: 'pipe',
-                },
-                {
-                    title: '制品库 Repo',
-                    value: 'repo',
-                },
-                {
-                    title: '测试用例 Test',
-                    value: 'test',
-                },
-            ],
-        },
-    ];
+const MenuDropdown: React.FC<MenuDropdownProps> = props => {
+    const { SubMenu } = Menu;
+    const { menuDate, handleMenuClick } = props;
 
     const menu = (
-        <Menu {...props}>
+        // @ts-ignore
+        <Menu {...props} onClick={handleMenuClick} >
             {
-                menuDate.map(item => {
+                menuDate && menuDate.map(item => {
                     if (item.child && item.child.length > 0) {
-                        return (<SubMenu title={item.title}>
-                            {item.child.map(({title, value}) => {
-                                return <Menu.Item key={value}>{title}</Menu.Item>;
+                        return (<SubMenu
+                            title={item.title}
+                            popupClassName={classNames(`${clsPrefix}-menu`, props.className)}
+                        >
+                            {item.child.map((item: any) => {
+                                return <Menu.Item key={item.value}>{item.title}</Menu.Item>;
                             })}
                         </SubMenu>);
                     } else {
@@ -77,10 +48,9 @@ const RightDropdown: React.FC<DropdownProps> = props => {
     return (<AntdDropdown
         // @ts-ignore
         overlay={menu}
-        trigger={['contextMenu']}
         className={classNames(clsPrefix, props.className)}
         {...props}
     />);
 };
 
-export default RightDropdown;
+export default MenuDropdown;
--- packages/ui/right-dropdown/stories/index.stories.tsx
+++ packages/ui/right-dropdown/stories/index.stories.tsx
@@ -8,17 +8,72 @@ export default {
 
 export const Demo = () => {
 
+    // Dropdown点击事件
     const click = (e: any) => {
+        console.log('Dropdown点击事件');
+        console.log(e);
+    };
+
+    const menuDate = [
+        {
+            title: '新建 Pull Request',
+            value: 'newPr',
+        },
+        {
+            title: '新建分支',
+            value: 'newBranch',
+        },
+        {
+            title: '新建组件',
+            value: 'newComponents',
+        },
+        {
+            title: '新建项目',
+            child: [
+                {
+                    title: '计划 Project',
+                    value: 'project',
+                },
+                {
+                    title: '文档 DOC',
+                    value: 'doc',
+                },
+                {
+                    title: '流水线 Pipe',
+                    value: 'pipe',
+                },
+                {
+                    title: '制品库 Repo',
+                    value: 'repo',
+                },
+                {
+                    title: '测试用例 Test',
+                    value: 'test',
+                },
+            ],
+        },
+    ];
+
+    // 菜单点击事件
+    const handleMenuClick = (e: any) => {
+        console.log('获取Menu点击的key值:' + e.key);
         console.log(e);
     };
 
     return (
         <div style={{padding: 30}}>
             <Divider orientation="left">基本</Divider>
-            {/* @ts-ignore */}
-            <RightDropdown onClick={click}>
+
+            <RightDropdown
+                //  @ts-ignore
+                trigger={['contextMenu']}
+                menuDate={menuDate}
+                onClick={click}
+                handleMenuClick={handleMenuClick}
+            >
                 <div
                     style={{
+                        width: '140px',
                         textAlign: 'center',
                         height: 200,
                         lineHeight: '200px',
