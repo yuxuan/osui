18b79cec00f20aa8cd65c18fcccbbc24d139f2a4
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2020-11-24 11:57:58 +0800

    fix: modal样式修复

--- packages/ui/docs/osui/stories/modal/index.stories.tsx
+++ packages/ui/docs/osui/stories/modal/index.stories.tsx
@@ -3,7 +3,7 @@ import Button from '@osui/button';
 import Modal from '@osui/modal';
 
 export default {
-    title: 'modal',
+    title: 'Modal',
 };
 
 export const Demo = () => {
--- packages/ui/modal/stories/osui.stories.tsx
+++ packages/ui/modal/stories/osui.stories.tsx
@@ -3,7 +3,7 @@ import Button from '@osui/button';
 import Modal from '../src';
 
 export default {
-    title: 'modal',
+    title: 'Modal',
 };
 
 export const Demo = () => {
@@ -43,4 +43,28 @@ export const Demo = () => {
                 何时使用：需要用户处理事务，又不希望跳转页面以致打断工作流程时，可以使用 Modal 在当前页面正中打开一个浮层，承载相应的操作。
             </Modal>
         </div>);
+};
+
+export const Confirm = () => {
+
+    function showConfirm() {
+        Modal.confirm({
+            size: 'small',
+            content: '选中的安全组绑定了其他实例，安全组删除后无法恢复！请确定是否要删除安全组"test"',
+            onOk() {
+                console.log('OK');
+            },
+            onCancel() {
+                console.log('Cancel');
+            },
+        });
+    }
+
+    return (
+        <>
+            <Button type="primary" onClick={() => showConfirm()}>
+                Confirm
+            </Button>
+        </>
+    );
 };
