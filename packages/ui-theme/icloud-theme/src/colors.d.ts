af377a278d8d7b35ee9b30cc3c02158f36e56621
otakustay <otakustay@gmail.com>
2020-11-24 17:51:57 +0800

    feat: 给theme增加类型定义

---
+++ packages/ui-theme/icloud-theme/src/colors.d.ts
@@ -0,0 +1,4 @@
+export const tagColors: Record<string, string>;
+export const rotatingColors: Record<string, string>;
+export const uiColors: Record<string, string>;
+export const presetColors: Record<string, string>;
---
+++ packages/ui-theme/icloud-theme/src/index.d.ts
@@ -0,0 +1,1 @@
+export const lightColors: Record<string, string>;
