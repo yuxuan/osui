bd36bdce939ca9f1f03e62ebd6b298ffa2b63205
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2020-12-16 13:43:16 +0800

    fix: timeline icloud 主题调整

--- packages/ui/timeline/stories/icloud.stories.tsx
+++ packages/ui/timeline/stories/icloud.stories.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {ClockCircleOutlined} from '@ant-design/icons';
 import Timeline from '../src';
 
 export default {
@@ -13,18 +12,6 @@ export const Demo = () => {
             <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
             <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
             <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-        </Timeline>
-        <Timeline mode="alternate" style={{width: 200}}>
-            <Timeline.Item>时间列表</Timeline.Item>
-            <Timeline.Item color="green">时间列表</Timeline.Item>
-            <Timeline.Item dot={<ClockCircleOutlined style={{fontSize: '16px'}} />}>
-                时间列表
-            </Timeline.Item>
-            <Timeline.Item color="red">时间列表</Timeline.Item>
-            <Timeline.Item>时间列表</Timeline.Item>
-            <Timeline.Item dot={<ClockCircleOutlined style={{fontSize: '16px'}} />}>
-                时间列表
-            </Timeline.Item>
         </Timeline>
     </div>);
 };
