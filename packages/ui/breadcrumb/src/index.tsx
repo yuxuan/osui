91bc001e812cda04df956b42d01c6d1766dfd47e
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2020-12-16 21:06:38 +0800

    fix: breadcrumb icloud实现

--- packages/ui/breadcrumb/src/index.tsx
+++ packages/ui/breadcrumb/src/index.tsx
@@ -1,2 +1,116 @@
-import {Breadcrumb} from 'antd';
+import React, { useReducer } from 'react';
+import { Breadcrumb as AntdBreadcrumb } from 'antd';
+import { BreadcrumbProps as AntdBrandcrumbProps } from 'antd/es/breadcrumb';
+import classNames from 'classnames';
+import './index.less';
+
+const clsPrefix = 'osui-breadcrumb';
+
+const Ellipsis = ({onClick}: {onClick: () => void}) => (
+    <span className={`${clsPrefix}-ellipsis`} onClick={onClick}>...</span>
+);
+
+interface EllipsisConfig {
+    shownLastItems: number; // 已经展示出来的最后的items数
+    onEllipsisClick: () => void;
+    maxItemLength: number;
+}
+type GetItemRenderWithEllipsis = (
+    render: AntdBrandcrumbProps['itemRender'],
+    config: EllipsisConfig
+) => AntdBrandcrumbProps['itemRender'];
+
+const getItemRenderWithEllipsis: GetItemRenderWithEllipsis = (render, config) => (route, params, routes, paths) => {
+    const {shownLastItems, onEllipsisClick} = config;
+
+    const index = routes.indexOf(route);
+    // 当展示第一个item 且 总长度 - 展示出来后n个 > 1 时，展示 ...
+    if (index === 1 && routes.length - shownLastItems > 1) {
+        return <Ellipsis onClick={onEllipsisClick} />;
+    }
+    // 隐藏掉 1 到 展示出来后n个之间的 item
+    if (index > 1 && index < routes.length - shownLastItems) {
+        return null;
+    }
+
+    return render && render(route, params, routes, paths);
+};
+
+const getChildrenWithEllipsis = (children: React.ReactNode | React.ReactNode[], config: EllipsisConfig) => {
+    const {shownLastItems, onEllipsisClick, maxItemLength} = config;
+    // 如果不是array，或者children长度小于maxItemLenght时，直接展示children
+    if (!Array.isArray(children) || children.length <= maxItemLength) {
+        return children;
+    }
+    // 如果剩余最后一个item没展示时，去掉Ellipsis，展示所有
+    if (children.length - shownLastItems <= 1) {
+        return children;
+    }
+    // 展示 第一个 ... 倒数两个
+    return [
+        children[0],
+        <AntdBreadcrumb.Item key="_ellipsis"><Ellipsis onClick={onEllipsisClick} /></AntdBreadcrumb.Item>,
+        ...children.slice(-shownLastItems),
+    ];
+};
+
+interface BreadcrumbProps extends AntdBrandcrumbProps {
+    /**
+     * @description 是否展示 ...
+     */
+    showEllipsis?: boolean;
+    /**
+     * @description 配合showEllipsis使用，当showEllipsis为true时，超过这个数字则会展示 ...
+     */
+    maxItemLength?: number;
+}
+
+interface BreadcrumbDecorator extends React.FC<BreadcrumbProps> {
+    Item: typeof AntdBreadcrumb.Item;
+    Separator: typeof AntdBreadcrumb.Separator;
+}
+
+const Breadcrumb: BreadcrumbDecorator = props => {
+    const {children, routes, itemRender, showEllipsis, className, maxItemLength = 5, ...restProps} = props;
+
+    const [shownLastItems, doPop] = useReducer<React.Reducer<number, void>>(
+        (index: number) => {
+            if (children && Array.isArray(children) && children.length - index > 1) {
+                return index + 1;
+            }
+            if (routes && routes.length - index > 1) {
+                return index + 1;
+            }
+            return 0;
+        },
+        2
+    );
+
+    const isShowEllipsis = showEllipsis
+        && (
+            (routes && routes.length > maxItemLength)
+            || (children && Array.isArray(children) && children.length > maxItemLength)
+        );
+
+    const config = {shownLastItems, onEllipsisClick: doPop, maxItemLength};
+
+    return (
+        <AntdBreadcrumb
+            className={classNames(clsPrefix, className)}
+            itemRender={
+                (isShowEllipsis && itemRender)
+                    ? getItemRenderWithEllipsis(itemRender, config)
+                    : itemRender
+            }
+            routes={routes}
+            {...restProps}
+        >
+            {(isShowEllipsis && children) ? getChildrenWithEllipsis(children, config) : children}
+        </AntdBreadcrumb>
+    );
+};
+
+Breadcrumb.Item = AntdBreadcrumb.Item;
+Breadcrumb.Separator = AntdBreadcrumb.Separator;
+
 export default Breadcrumb;
--- packages/ui/breadcrumb/stories/icloud.stories.tsx
+++ packages/ui/breadcrumb/stories/icloud.stories.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
+import Menu from '@osui/menu';
 import Breadcrumb from '../src';
 
 export default {
@@ -8,14 +10,143 @@ export default {
 export const Demo = () => {
     return (
         <Breadcrumb>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>一级页面名称</Breadcrumb.Item>
             <Breadcrumb.Item>
-                <a href="">Application Center</a>
+                <a href="">二级页面名称</a>
             </Breadcrumb.Item>
             <Breadcrumb.Item>
-                <a href="">Application List</a>
+                <a href="">三级页面名称</a>
             </Breadcrumb.Item>
-            <Breadcrumb.Item>An Application</Breadcrumb.Item>
+            <Breadcrumb.Item>
+                <a href="">四级页面名称</a>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+                <a href="">末级页面名称</a>
+            </Breadcrumb.Item>
+        </Breadcrumb>
+    );
+};
+
+export const Ellipsis = () => {
+    return (
+        <>
+            <p>当showEllipsis为true，且长度超过5个时，展示【...】</p>
+            <p>点击【...】可以从后面展示被隐藏的面包屑</p>
+            <p>Demo: </p>
+            <br />
+            <Breadcrumb showEllipsis>
+                <Breadcrumb.Item>一级页面名称</Breadcrumb.Item>
+                <Breadcrumb.Item>
+                    <a href="">二级页面名称</a>
+                </Breadcrumb.Item>
+                <Breadcrumb.Item>
+                    <a href="">三级页面名称</a>
+                </Breadcrumb.Item>
+                <Breadcrumb.Item>四级页面名称</Breadcrumb.Item>
+                <Breadcrumb.Item>五级页面名称</Breadcrumb.Item>
+                <Breadcrumb.Item>六级页面名称</Breadcrumb.Item>
+                <Breadcrumb.Item>末级页面名称</Breadcrumb.Item>
+            </Breadcrumb>
+        </>
+    );
+};
+
+export const Routes = () => {
+    const routes = [
+        {
+            path: 'index1',
+            breadcrumbName: 'home',
+        },
+        {
+            path: 'index2',
+            breadcrumbName: 'home2',
+        },
+        {
+            path: 'index3',
+            breadcrumbName: 'home3',
+        },
+        {
+            path: 'index4',
+            breadcrumbName: 'home4',
+        },
+        {
+            path: 'index5',
+            breadcrumbName: 'home5',
+        },
+        {
+            path: 'first6',
+            breadcrumbName: 'first',
+            children: [
+                {
+                    path: '/general',
+                    breadcrumbName: 'General',
+                },
+                {
+                    path: '/layout',
+                    breadcrumbName: 'Layout',
+                },
+                {
+                    path: '/navigation',
+                    breadcrumbName: 'Navigation',
+                },
+            ],
+        },
+        {
+            path: 'second',
+            breadcrumbName: 'second',
+        },
+    ];
+
+    function itemRender(route, params, routes, paths) {
+        const last = routes.indexOf(route) === routes.length - 1;
+        return last ? (
+            <span>{route.breadcrumbName}</span>
+        ) : (
+            <Link to={paths.join('/')}>{route.breadcrumbName}</Link>
+        );
+    }
+
+    return (
+        <BrowserRouter>
+            <p>支持routes</p>
+            <Breadcrumb itemRender={itemRender} routes={routes} showEllipsis />
+        </BrowserRouter>
+    );
+};
+
+
+export const Dropdown = () => {
+
+    const menu = (
+        <Menu>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+                    General
+                </a>
+            </Menu.Item>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+                    Layout
+                </a>
+            </Menu.Item>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+                    Navigation
+                </a>
+            </Menu.Item>
+        </Menu>
+    );
+
+    return (
+        <Breadcrumb>
+            <Breadcrumb.Item>Ant Design</Breadcrumb.Item>
+            <Breadcrumb.Item>
+                <a href="">Component</a>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item overlay={menu}>
+                <a href="">General</a>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>Button</Breadcrumb.Item>
         </Breadcrumb>
     );
 };
