f672a60ab9bb86f97796a54f2779c65abdad394e
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2020-12-29 20:25:51 +0800

    fix: steps样式调整

--- packages/ui/steps/src/index.tsx
+++ packages/ui/steps/src/index.tsx
@@ -1,2 +1,20 @@
-import {Steps} from 'antd';
+import React from 'react';
+import {Steps as AntdSteps} from 'antd';
+import {StepsProps as AntdStepsProps} from 'antd/es/steps';
+import classNames from 'classnames';
+import './index.less';
+
+const clsPrefix = 'osui-steps';
+
+interface StepsInterface extends React.FC<AntdStepsProps> {
+    Step: typeof AntdSteps.Step;
+}
+
+const Steps: StepsInterface = ({className, ...props}) => {
+    const innerClassName = classNames(className, clsPrefix);
+    return <AntdSteps className={innerClassName} {...props} />;
+};
+
+Steps.Step = AntdSteps.Step;
+
 export default Steps;
