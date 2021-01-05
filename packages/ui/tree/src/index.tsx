f75be55449a1d5f1d1e38aaa9c9649dafc83f67b
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-01-05 15:43:41 +0800

    fix: tree样式的更新

--- packages/ui/tree/src/index.tsx
+++ packages/ui/tree/src/index.tsx
@@ -1,2 +1,30 @@
-import {Tree} from 'antd';
+import React from 'react';
+import {Tree as AntdTree} from 'antd';
+import {TreeProps as AntdTreeProps} from 'antd/es/tree';
+import classNames from 'classnames';
+import './index.less';
+
+const clsPrefix = 'osui-tree';
+
+export interface TreeProps extends AntdTreeProps {
+    size?: 'small' | 'middle' | 'large';
+}
+interface CompoundedComponent extends React.ForwardRefExoticComponent<TreeProps & React.RefAttributes<any>> {
+    TreeNode: typeof AntdTree.TreeNode;
+    DirectoryTree: typeof AntdTree.DirectoryTree;
+}
+
+
+const Tree = React.forwardRef<any, TreeProps>(({className, size, ...props}, ref) => {
+    const innerClassName = classNames(
+        className,
+        clsPrefix,
+        {[`${clsPrefix}-${size}`]: size}
+    );
+    return <AntdTree ref={ref} className={innerClassName} {...props} />;
+}) as CompoundedComponent;
+
+Tree.TreeNode = AntdTree.TreeNode;
+Tree.DirectoryTree = AntdTree.DirectoryTree;
+
 export default Tree;
