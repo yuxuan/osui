e11a745d0579586fc70380b8b12ec7be3fb17ed9
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-01-22 22:16:17 +0800

    fix: collapse arrow样式修复

--- packages/ui/collapse/.storybook/preview.js
+++ packages/ui/collapse/.storybook/preview.js
@@ -2,7 +2,7 @@ import {addParameters} from '@storybook/react';
 import {DocsPage, DocsContainer} from '@storybook/addon-docs/blocks';
 import {
     INITIAL_VIEWPORTS,
-  } from '@storybook/addon-viewport';
+} from '@storybook/addon-viewport';
 import '@osui/theme/dist/theme/vars.css';
 // import '@osui/icloud-theme/dist/theme/vars.css';
 
