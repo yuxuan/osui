5fbd1a5f77fab7174b8d85b9bf6767eb0a29c8c2
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-01-24 18:33:26 +0800

    fix: 调整徽标

--- packages/ui/badge/src/index.tsx
+++ packages/ui/badge/src/index.tsx
@@ -7,14 +7,24 @@ import './index.less';
 const clsPrefix = 'osui-badge';
 
 export interface BadgeProps extends AntdBadgeProps {
+    /**
+     * @description 来展示徽标的不同状态
+     */
     type?: 'success' | 'warning' | 'error';
+    /**
+     * @description 是否仅展示count
+     */
+    countOnly?: boolean;
 }
 
 const Badge: React.FC<BadgeProps> = props => {
     const innerClassName = classNames(
         props.className,
         clsPrefix,
-        {[`${clsPrefix}-type-${props.type}`]: props.type}
+        {
+            [`${clsPrefix}-type-${props.type}`]: props.type,
+            [`${clsPrefix}-count-only`]: props.countOnly,
+        }
     );
 
     return (<AntdBadge {...props} className={innerClassName} />);
