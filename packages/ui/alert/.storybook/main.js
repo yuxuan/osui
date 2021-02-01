0c157572b01ca7c7958675e9a56b1736f55b9352
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-02-01 13:47:34 +0800

    chore: alert试点icon, theme dependency icon

--- packages/ui/alert/.storybook/main.js
+++ packages/ui/alert/.storybook/main.js
@@ -73,6 +73,9 @@ module.exports = {
 
         config.resolve.extensions.push('.ts', '.tsx');
         config.resolve.alias['@'] = path.resolve(__dirname, '../src');
+        if (!isOsuiTheme) {
+            config.resolve.alias['@osui/icons'] = require.resolve('@osui/icons-icloud');
+        }
         return config;
     },
 };
