d7d7e09d48feecefaea7762b6a1c4e1b948f111d
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-02-02 17:15:51 +0800

    fix: cascader 调整

--- packages/ui/cascader/src/index.tsx
+++ packages/ui/cascader/src/index.tsx
@@ -1,15 +1,47 @@
 import React from 'react';
-import {Cascader as AntdCascader} from 'antd';
-import {CascaderProps as AntdCascaderProps} from 'antd/es/cascader';
+import { Cascader as AntdCascader } from 'antd';
+import { CascaderProps as AntdCascaderProps } from 'antd/es/cascader';
 import classNames from 'classnames';
+import { IconDownOutlined, IconRightOutlined } from '@osui/icons';
 import './index.less';
 
 const clsPrefix = 'osui-cascader';
 
-const Cascader: React.FC<AntdCascaderProps> = ({className, displayRender, ...props}) => {
+const Cascader: React.FC<AntdCascaderProps> = ({
+    className,
+    popupClassName,
+    expandIcon,
+    suffixIcon,
+    displayRender,
+    ...props
+}) => {
     const innerClassName = classNames(className, clsPrefix);
-    const innerDisplayRender = displayRender ?? ((label: string[]) => label.join(' > '));
-    return <AntdCascader className={innerClassName} displayRender={innerDisplayRender} {...props} />;
+    const innerPopupClassName = classNames(popupClassName, `${clsPrefix}-menu`);
+    const innerDisplayRender = displayRender ?? (
+        (label: string[]) => label.map(
+            (item, index) => (
+                index === label.length - 1 // 最后一个后面不加icon
+                    ? (<span>{item}</span>)
+                    : (
+                        <>
+                            <span>{item}</span><IconRightOutlined className={`${clsPrefix}-label-divider-icon`} />
+                        </>
+                    )
+            )
+        )
+    );
+    const innerExpandIcon = expandIcon ?? <IconRightOutlined />;
+    const innerSuffixIcon = suffixIcon ?? <IconDownOutlined />;
+    return (
+        <AntdCascader
+            className={innerClassName}
+            displayRender={innerDisplayRender}
+            popupClassName={innerPopupClassName}
+            expandIcon={innerExpandIcon}
+            suffixIcon={innerSuffixIcon}
+            {...props}
+        />
+    );
 };
 
 export default Cascader;
