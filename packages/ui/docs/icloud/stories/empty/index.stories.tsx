cfca7d147291fb40090b77dccfca0b65e94401d7
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-02-18 20:04:46 +0800

    docs: empty
    Change-Id: I4719e69905f4bf9a7c4cc5747f3bfcb40b546eb6

--- packages/ui/docs/icloud/stories/empty/index.stories.tsx
+++ packages/ui/docs/icloud/stories/empty/index.stories.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import {Empty} from 'antd';
+import Empty from '@osui/empty';
 
 export default {
     title: '验收中/Empty 空状态',
     component: Empty,
 };
 
-export const Demo = () => {
+export const Demo1 = () => {
     return (
         <Empty />
+    );
+};
+
+export const Demo2 = () => {
+    return (
+        <Empty type="404" />
+    );
+};
+
+export const Demo3 = () => {
+    return (
+        <Empty type="error" />
     );
 };
