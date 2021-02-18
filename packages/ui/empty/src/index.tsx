c2ca97e73b8516001c2953c444f1851992ce9378
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-02-18 18:00:03 +0800

    fix: empty
    Change-Id: I01854096bab3e7e26086b17dda78a766935911db

--- packages/ui/empty/src/index.tsx
+++ packages/ui/empty/src/index.tsx
@@ -1,2 +1,39 @@
-import {Empty} from 'antd';
+import React, {ImgHTMLAttributes} from 'react';
+import {Empty as AntdEmpty} from 'antd';
+import {EmptyProps as AntdEmptyProps} from 'antd/es/empty';
+import classNames from 'classnames';
+import ImageEmpty from './empty.svg';
+import Image404 from './404.svg';
+import ImageError from './error.svg';
+import './index.less';
+
+const clsPrefix = 'osui-empty';
+
+export const IMAGE_EMPTY = (props: ImgHTMLAttributes<unknown>) => (<img src={ImageEmpty} {...props} />);
+export const IMAGE_404 = (props: ImgHTMLAttributes<unknown>) => (<img src={Image404} {...props} />);
+export const IMAGE_ERROR = (props: ImgHTMLAttributes<unknown>) => (<img src={ImageError} {...props} />);
+
+export interface EmptyProps extends AntdEmptyProps {
+    type?: 'empty' | 'error' | '404';
+}
+const Empty: React.FC<EmptyProps> = ({className, image, type, ...props}) => {
+    const innerClassName = classNames(className, clsPrefix);
+    let Image = IMAGE_EMPTY;
+    switch (type) {
+        case 'empty':
+            Image = IMAGE_EMPTY;
+            break;
+        case 'error':
+            Image = IMAGE_ERROR;
+            break;
+        case '404':
+            Image = IMAGE_404;
+            break;
+        default:
+            Image = IMAGE_EMPTY;
+    }
+    const innerImage = image ?? <Image />;
+    return <AntdEmpty className={innerClassName} {...props} image={innerImage} />;
+};
+
 export default Empty;
--- packages/ui/empty/stories/osui.stories.tsx
+++ packages/ui/empty/stories/osui.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Empty} from 'antd';
+import Empty from '../src';
 
 export default {
     title: 'Empty',
