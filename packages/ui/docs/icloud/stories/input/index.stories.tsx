0f0481e6263acbf82ae708bc19fbca53c8fb7d3c
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-02-28 15:08:07 +0800

    docs: update
    Change-Id: I3b8c9d1a91fb52c972145433a6b1b7cc33885c0a

--- packages/ui/docs/icloud/stories/input/index.stories.tsx
+++ packages/ui/docs/icloud/stories/input/index.stories.tsx
@@ -34,6 +34,7 @@ export const Demo = () => {
             <TextArea style={{ width: 500, height: 150 }} placeholder="请输入" />
             <br />
             <br />
+            <Input.Password style={{ width: 500 }} placeholder="请输入" />
         </>);
 };
 
--- packages/ui/docs/icloud/stories/time-picker/index.stories.tsx
+++ packages/ui/docs/icloud/stories/time-picker/index.stories.tsx
@@ -24,5 +24,3 @@ export const Demo2 = () => {
         </>
     );
 };
-
-
