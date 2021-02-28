80c3e6471ce501533b85af50110f950332490104
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-02-28 15:05:53 +0800

    fix: modal滚动条margin换padding
    Change-Id: I2f96f5c3acd23003031352df6626de01a8fb04ed

--- packages/ui-theme/icloud-theme/src/colors.js
+++ packages/ui-theme/icloud-theme/src/colors.js
@@ -473,8 +473,8 @@ exports.presetColors = {
     '--modal-close-icon-line-height': '40px',
     '--modal-body-border-bottom': '1px solid var(--theme-border-color-split)',
     '--modal-footer-border-top': 'none',
-    '--modal-body-padding': '20px 0px',
-    '--modal-body-margin': '0px 20px',
+    '--modal-body-padding': '20px 20px',
+    '--modal-body-margin': '0px',
     // 计算公示：100vh - header高度 - footer高度 - 上下要留50px
     '--modal-body-max-height': 'calc(100vh - 40px - 60px - 100px)',
     '--modal-close-icon-margin-right': '20px',
