f9c297ca96c3a02bca934f2e4c40b80feaf8fb29
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-03-28 21:31:37 +0800

    fix: input增加Input.Search支持icon的形式
    Change-Id: If5fe67e8210b8144551b4ca5ca8be5787cf81a6b

--- packages/ui/docs/icloud/stories/input-search/index.stories.tsx
+++ packages/ui/docs/icloud/stories/input-search/index.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Input from '@osui/input';
 import Divider from '@osui/divider';
-import { IconSearchOutlined } from '@osui/icons';
+import BrandProvider from '@osui/brand-provider';
 
 export default {
     title: '数据录入/Input 搜索框',
@@ -17,43 +17,51 @@ export const Demo = () => {
             <p>当数据量庞大并且结构复杂时，建议为用户提供搜索功能，让用户可以快速找到想要的信息。</p>
             <Divider>展示</Divider>
             <p><strong>FE说明：</strong>下面demo使用的是Input + 后缀icon，注意：要加allowClear</p>
-            <Input
+            <Input.Search
                 allowClear
+                withSuffixIcon
                 style={{ width: 200 }}
                 placeholder="请输入搜索关键字"
-                suffix={
-                    <IconSearchOutlined />
-                }
+                onSearch={console.log}
             />
             <br />
             <br />
-            <Input
+            <Input.Search
                 disabled
+                withSuffixIcon
                 style={{ width: 200 }}
                 placeholder="请输入搜索关键字"
-                suffix={
-                    <IconSearchOutlined />
-                }
+                onSearch={console.log}
             />
             <br />
             <br />
             <p><strong>FE说明：</strong>下面demo使用的是Input.Search</p>
             <Input.Search
+                allowClear
                 style={{ width: 400 }}
                 placeholder="input search text"
-                allowClear
                 enterButton="搜索"
                 onSearch={onSearch}
             />
             <br />
             <br />
             <Input.Search
+                allowClear
+                disabled
                 style={{ width: 400 }}
                 placeholder="input search text"
-                allowClear
                 enterButton="搜索"
-                disabled
             />
+            <br />
+            <br />
+            <BrandProvider brand="icloud">
+                <Input.Search
+                    allowClear
+                    style={{ width: 400 }}
+                    placeholder="input search text"
+                    onSearch={console.log}
+                />
+            </BrandProvider>
         </>
     );
 };
--- packages/ui/input/src/index.tsx
+++ packages/ui/input/src/index.tsx
@@ -1,5 +1,8 @@
-import React, {useCallback, useState} from 'react';
-import {Input as AntdInput} from 'antd';
+import React, { useCallback, useState, useRef, useMemo } from 'react';
+import { Input as AntdInput } from 'antd';
+import {composeRef} from 'rc-util/lib/ref';
+import {useBrandContext} from '@osui/brand-provider';
+import {IconSearchOutlined} from '@osui/icons';
 import {
     InputProps as AntdInputProps,
     TextAreaProps as AntdTextAreaProps,
@@ -14,12 +17,12 @@ export type InputProps = AntdInputProps;
 
 interface InputInterface extends React.FC<InputProps> {
     Group: typeof AntdInput.Group;
-    Search: typeof AntdInput.Search;
+    Search: typeof Search;
     TextArea: any;
     Password: typeof AntdInput.Password;
 }
 
-const Input = React.forwardRef<any, AntdInputProps>(({className, onFocus, onBlur, disabled, ...props}, ref) => {
+const Input = React.forwardRef<any, AntdInputProps>(({ className, onFocus, onBlur, disabled, ...props }, ref) => {
     const [focused, setFocused] = useState(false);
     const innerClassNames = classNames(clsPrefix, className, {
         [`${clsPrefix}-focused`]: focused,
@@ -60,36 +63,118 @@ Input.TextArea = React.forwardRef<any, AntdTextAreaProps>((props, ref) => {
 });
 
 Input.Group = AntdInput.Group;
-Input.Search = React.forwardRef<any, AntdSearchProps>(({className, disabled, onBlur, onFocus, ...props}, ref) => {
-    const [focused, setFocused] = useState(false);
 
-    const innerClassNames = classNames(clsPrefix, className, {
-        [`${clsPrefix}-focused`]: focused,
-        [`${clsPrefix}-disabled`]: disabled,
-    });
-    const handleFocus = useCallback(
-        e => {
-            onFocus && onFocus(e);
-            setFocused(true);
-        },
-        [onFocus]
-    );
-    const handleBlur = useCallback(
-        e => {
-            onBlur && onBlur(e);
-            setFocused(false);
-        },
-        [onBlur]
-    );
-    return (
-        <AntdInput.Search
-            ref={ref}
-            {...props}
-            className={classNames(clsPrefix, innerClassNames)}
-            disabled={disabled}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-        />);
-});
+export interface SearchProps extends AntdSearchProps {
+    withSuffixIcon?: boolean;
+}
+
+const Search = React.forwardRef<any, SearchProps>(
+    ({
+        className,
+        disabled,
+        onBlur,
+        onFocus,
+        onChange,
+        onSearch,
+        withSuffixIcon,
+        suffix,
+        ...props
+    },
+    ref
+    ) => {
+        const inputRef = useRef(null);
+        const {brand} = useBrandContext();
+
+        const [focused, setFocused] = useState(false);
+
+        const innerClassNames = classNames(clsPrefix, className, {
+            [`${clsPrefix}-focused`]: focused,
+            [`${clsPrefix}-disabled`]: disabled,
+        });
+        const innerWithSuffixIcon = withSuffixIcon ?? brand === 'icloud';
+        const handleFocus = useCallback(
+            e => {
+                onFocus && onFocus(e);
+                setFocused(true);
+            },
+            [onFocus]
+        );
+        const handleBlur = useCallback(
+            e => {
+                onBlur && onBlur(e);
+                setFocused(false);
+            },
+            [onBlur]
+        );
+        const handleSearch = useCallback(
+            e => {
+                onSearch && onSearch((inputRef.current as any).input.value, e);
+            },
+            [onSearch]
+        );
+        const handleClick = useCallback(
+            e => {
+                handleSearch(e);
+            },
+            [handleSearch]
+        );
+        const handleChange = useCallback(
+            e => {
+                // 点击清除时也要调用onSearch
+                if (e && e.target && e.type === 'click' && onSearch) {
+                    onSearch(e.target.value, e);
+                }
+                if (onChange) {
+                    onChange(e);
+                }
+            },
+            [onSearch, onChange]
+        );
+        // 处理搜索图标，如果没有传入的话，默认是搜索icon。TODO： loading的支持
+        const innerSuffix = useMemo(
+            () => {
+                let icon = suffix;
+                if (innerWithSuffixIcon && typeof icon === 'undefined') {
+                    icon = <IconSearchOutlined />;
+                }
+                if (React.isValidElement(icon)) {
+                    return React.cloneElement(
+                        icon,
+                        {
+                            className: `${clsPrefix}-search-icon`,
+                            onClick: handleClick,
+                        }
+                    );
+                }
+                return icon;
+            },
+            [suffix, innerWithSuffixIcon, handleClick]
+        );
+        if (innerWithSuffixIcon) {
+            return (
+                <AntdInput
+                    ref={composeRef(inputRef, ref)}
+                    {...props}
+                    className={classNames(clsPrefix, innerClassNames)}
+                    disabled={disabled}
+                    suffix={innerSuffix}
+                    onPressEnter={handleSearch}
+                    onChange={handleChange}
+                />
+            );
+        }
+        return (
+            <AntdInput.Search
+                ref={ref}
+                {...props}
+                className={classNames(clsPrefix, innerClassNames)}
+                disabled={disabled}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+            />);
+    }
+);
+
+Input.Search = Search;
 
 export default Input;
