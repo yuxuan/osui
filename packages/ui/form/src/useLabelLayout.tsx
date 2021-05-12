32f47217d6babe2a37768425034a03ba459d990e
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-05-12 10:34:02 +0800

    fix: 调整Form.useLabelLayout
    Change-Id: I7efb68b36b985af866a19af024a8564d923b95dd

--- packages/ui/form/src/useLabelLayout.tsx
+++ packages/ui/form/src/useLabelLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useLayoutEffect, useRef} from 'react';
 const labelMarginRight = 20;
 /**
  * @description 用来实现icloud规范的布局，即label右侧间距20px，label不flex，内容区域flex，label以最长宽度内容或提供最长宽度为最长宽度。
@@ -6,75 +6,95 @@ const labelMarginRight = 20;
  * @param maxWidth 可选，设置最大宽度
  *
  */
-export default (formName: string, maxWidth?: number) => React.useLayoutEffect(
-    () => {
-        let actualLabel = null;
-        let antFormItemLabel = null;
-        let antFormItemControl = null;
-
-        try {
-            // 确保select的到
-            actualLabel = document.querySelectorAll(
+export default (formName: string, maxWidth?: number) => {
+    const labelRef = useRef<NodeListOf<Element> | undefined >(undefined);
+    const antdFormLabelRef = useRef<NodeListOf<Element> | undefined >(undefined);
+    const antdFormItemControlRef = useRef<NodeListOf<Element> | undefined >(undefined);
+    const init = useRef(true);
+    useLayoutEffect(
+        () => {
+            labelRef.current = document.querySelectorAll(
                 `#${formName} .ant-form-item > .ant-form-item-label > label`
             );
-            antFormItemLabel = document.querySelectorAll(
+            antdFormLabelRef.current = document.querySelectorAll(
                 `#${formName} .ant-form-item > .ant-form-item-label`
             );
-            antFormItemControl = document.querySelectorAll(
+            antdFormItemControlRef.current = document.querySelectorAll(
                 `#${formName} .ant-form-item > .ant-form-item-control:first-child`
             );
-        }
-        catch (e) {
-            return;
-        }
+        },
+        [formName]
+    );
 
-        let innerMaxWidth = 0;
-        if (maxWidth) {
-            innerMaxWidth = maxWidth;
-        }
-        else {
-            actualLabel.forEach(element => {
-                if (element.clientWidth > innerMaxWidth) {
-                    innerMaxWidth = element.clientWidth - 8; // 减去冒号后面的8px margin
-                }
-            });
-        }
+    useLayoutEffect(
+        () => {
+            const currentLabels = document.querySelectorAll(
+                `#${formName} .ant-form-item > .ant-form-item-label > label`
+            );
+            const currentAntdFormItemLabels = document.querySelectorAll(
+                `#${formName} .ant-form-item > .ant-form-item-label`
+            );
 
-        const fullWidth = innerMaxWidth + labelMarginRight;
-
-        antFormItemLabel.forEach(
-            element => {
-                element.setAttribute(
-                    'style',
-                    `max-width: ${fullWidth}px; flex: 0 0 ${fullWidth}px;`
-                );
+            if (!labelRef.current || !antdFormLabelRef.current || !antdFormItemControlRef.current) {
+                return;
             }
-        );
 
-        // 检测是否有overflow
-        antFormItemLabel.forEach(
-            element => {
-                if (element.scrollWidth > element.clientWidth) {
+            // 如果item没有变，不做额外操作
+            if (!init && currentLabels.length === labelRef.current.length) {
+                return;
+            }
+
+            init.current = false;
+            labelRef.current = currentLabels;
+            antdFormLabelRef.current = currentAntdFormItemLabels;
+
+            let innerMaxWidth = 0;
+            if (maxWidth) {
+                innerMaxWidth = maxWidth;
+            }
+            else {
+                labelRef.current.forEach(element => {
+                    if (element.clientWidth > innerMaxWidth) {
+                        innerMaxWidth = element.clientWidth - 8; // 减去冒号后面的8px margin
+                    }
+                });
+            }
+
+            const fullWidth = innerMaxWidth + labelMarginRight;
+
+            antdFormLabelRef.current.forEach(
+                element => {
                     element.setAttribute(
                         'style',
-                        // 覆盖掉white-space: nowrap;
-                        `max-width: ${fullWidth}px; flex: 0 0 ${fullWidth}px; white-space: initial;`
-                    );
-                    // 子label加margin，覆盖掉display: inline-flex;
-                    element.firstElementChild?.setAttribute(
-                        'style',
-                        `margin-right: ${labelMarginRight}px; display: inline-block;`
+                        `max-width: ${fullWidth}px; flex: 0 0 ${fullWidth}px;`
                     );
                 }
-            }
-        );
+            );
 
-        // 最后的footer内容
-        antFormItemControl.forEach(
-            element => {
-                element.setAttribute('style', `margin-left: ${fullWidth}px;`);
-            }
-        );
-    },
-    [formName, maxWidth]
-);
+            // 检测是否有overflow
+            antdFormLabelRef.current.forEach(
+                element => {
+                    if (element.scrollWidth > element.clientWidth) {
+                        element.setAttribute(
+                            'style',
+                            // 覆盖掉white-space: nowrap;
+                            `max-width: ${fullWidth}px; flex: 0 0 ${fullWidth}px; white-space: initial;`
+                        );
+                        // 子label加margin，覆盖掉display: inline-flex;
+                        element.firstElementChild?.setAttribute(
+                            'style',
+                            `margin-right: ${labelMarginRight}px; display: inline-block;`
+                        );
+                    }
+                }
+            );
+
+            // 最后的footer内容
+            antdFormItemControlRef.current.forEach(
+                element => {
+                    element.setAttribute('style', `margin-left: ${fullWidth}px;`);
+                }
+            );
+        }
+    );
+};
