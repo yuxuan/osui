0fe715bd6a2abaf0c6e2d4ba4fb8c3ab11088dce
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-05-20 17:11:42 +0800

    docs: link文档调整
    Change-Id: I07247adf44b1bb7689e64d4bac5e2786615d0525

--- packages/ui/docs/icloud/stories/a/index.stories.tsx
+++ packages/ui/docs/icloud/stories/a/index.stories.tsx
@@ -6,6 +6,19 @@ export default {
     title: '通用/超链接',
 };
 
+const Blockquote = ({children}) => (
+    <blockquote style={{
+        background: 'var(--color-brand-1)',
+        borderRadius: '3px',
+        borderLeft: '5px solid var(--color-brand-6)',
+        margin: '30px 0',
+        padding: '30px',
+    }}
+    >
+        {children}
+    </blockquote>
+);
+
 export const Demo = () => {
     const cssCode = `
     .text-link {
@@ -23,24 +36,34 @@ export const Demo = () => {
     .text-link:disabled {
         color: var(--theme-disabled-color);
     }
+    // 加强样式disabled颜色
+    .text-link-strong:disabled {
+        color: var(--color-brand-3);
+    }
     `;
 
     return (
         <>
-            <p><strong>FE说明：</strong>组件库中不包含A组件，文档只展示a的样式</p>
-            <p><strong>FE说明：</strong>项目中建议使用<a href="https://github.com/ecomfe/react-omni-link">react-omni-link</a></p>
+            <Blockquote>
+                组件库中不包含A组件，文档只展示a的样式
+            </Blockquote>
+            <Blockquote>
+                项目中建议使用<a href="https://github.com/ecomfe/react-omni-link">react-omni-link</a>
+            </Blockquote>
             <Divider>展示</Divider>
+            <Blockquote>
+                没有单独加组件封装，见css实现方式
+            </Blockquote>
             <h3>普通样式</h3>
-            <p><strong>FE说明：</strong>没有单独加组件封装，见css实现方式</p>
             <a href="www.baidu.com" className="text-link">www.baidu.com</a>
+            <h3>普通样式disabled</h3>
+            <a href="www.baidu.com" disabled>www.baidu.com</a>
             <br />
             <br />
             <h3>加强样式</h3>
             <a href="www.baidu.com">www.baidu.com</a>
-            <br />
-            <br />
-            <h3>disabled样式</h3>
-            <a href="www.baidu.com" disabled>www.baidu.com</a>
+            <h3>加强样式disabled</h3>
+            <a href="www.baidu.com" disabled style={{color: 'var(--color-brand-3)'}} >www.baidu.com</a>
             <Divider>普通样式css代码</Divider>
             <pre>
                 {/* eslint-disable-next-line react/no-danger */}
