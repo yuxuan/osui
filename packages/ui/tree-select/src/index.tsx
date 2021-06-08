fcdfa4d4102216c882be245a1a715f5ecaaf849e
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-06-08 16:12:34 +0800

    fix: tree-select type
    Change-Id: I75e3c1c58c2b9a8e0a65d90d8cbf9e3b3aa3d8a7

--- packages/ui/tree-select/src/index.tsx
+++ packages/ui/tree-select/src/index.tsx
@@ -1,20 +1,44 @@
 import React from 'react';
 import {TreeSelect as AntdTreeSelect} from 'antd';
-import {TreeSelectProps as AntdTreeSelectProps} from 'antd/es/';
+import {DefaultValueType} from 'rc-tree-select/lib/interface';
+import {TreeSelectProps as AntdTreeSelectProps, RefTreeSelectProps} from 'antd/lib/tree-select';
 import classNames from 'classnames';
 import hoistNonReactStatics from 'hoist-non-react-statics';
 import './index.less';
 
 const clsPrefix = 'osui-tree-select';
 
-export type TreeSelectFC = <T>(props: AntdTreeSelectProps<T>) => React.ReactElement;
-
-const TreeSelect: TreeSelectFC = ({className, dropdownClassName, ...props}) => {
+const InternalTreeSelect = <T extends DefaultValueType>(
+    {className, dropdownClassName, ...props}: AntdTreeSelectProps<T>,
+    ref: React.Ref<RefTreeSelectProps>
+) => {
     const innerClassName = classNames(className, clsPrefix);
     const innerDropdownClassName = classNames(dropdownClassName, `${clsPrefix}-dropdown`);
-    return <AntdTreeSelect className={innerClassName} dropdownClassName={innerDropdownClassName} {...props} />;
+    return (
+        <AntdTreeSelect
+            ref={ref}
+            className={innerClassName}
+            dropdownClassName={innerDropdownClassName}
+            {...props}
+        />
+    );
 };
 
-hoistNonReactStatics(TreeSelect, AntdTreeSelect);
+const TreeSelectRef = React.forwardRef(InternalTreeSelect) as <T extends DefaultValueType>(
+    props: AntdTreeSelectProps<T> & { ref?: React.Ref<RefTreeSelectProps> },
+) => React.ReactElement;
+
+// 提升TreeSelect属性
+hoistNonReactStatics(TreeSelectRef, TreeSelectRef);
+
+// 类型定义
+type InternalTreeSelectType = typeof TreeSelectRef;
+export interface TreeSelectInterface extends InternalTreeSelectType {
+    TreeNode: typeof AntdTreeSelect.TreeNode;
+    SHOW_ALL: typeof AntdTreeSelect.SHOW_ALL;
+    SHOW_PARENT: typeof AntdTreeSelect.SHOW_PARENT;
+    SHOW_CHILD: typeof AntdTreeSelect.SHOW_CHILD;
+}
+const TreeSelect = TreeSelectRef as TreeSelectInterface;
 
 export default TreeSelect;
