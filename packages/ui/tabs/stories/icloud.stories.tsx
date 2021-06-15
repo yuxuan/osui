9835c5e6326ebd23f943940e3dc08ee602af71aa
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-06-15 19:11:58 +0800

    fix: 调整tab
    Change-Id: I64c7ba3c624b36d38d7571b6a61b459ec8e4f4d2

--- packages/ui/tabs/stories/icloud.stories.tsx
+++ packages/ui/tabs/stories/icloud.stories.tsx
@@ -1,8 +1,26 @@
+/* eslint-disable react/no-multi-comp */
 import React from 'react';
-import { ForkOutlined } from '@ant-design/icons';
-import { IconCloseOutlined } from '@osui/icons';
+import { IconCloseOutlined, IconHomeOutlined, IconDownOutlined, IconPlusOutlined } from '@osui/icons';
 import Divider from '@osui/divider';
+import TextOverflowTooltip from '@osui/text-overflow-tooltip';
+import Dropdown from '@osui/dropdown';
+import FlexCentered from '@osui/flex-centered';
+import Menu from '@osui/menu';
+import Button from '@osui/button';
 import Tabs from '../src';
+
+const Blockquote = ({children}) => (
+    <blockquote style={{
+        background: 'var(--color-brand-1)',
+        borderRadius: '3px',
+        borderLeft: '5px solid var(--color-brand-6)',
+        margin: '30px 0',
+        padding: '30px',
+    }}
+    >
+        {children}
+    </blockquote>
+);
 
 export default {
     title: '数据展示/Tabs 标签页',
@@ -23,12 +41,32 @@ export const Demo = () => {
                     Content of Tab Pane 3
                 </TabPane>
             </Tabs>
-
+            <h3>tab显示不下可使用…，hover展示全部名称</h3>
+            <p>名称最多显示8个字(width: 130px)，超过显示… </p>
+            <Tabs defaultActiveKey="1" style={{ marginBottom: 50 }}>
+                <TabPane
+                    tab={(
+                        <TextOverflowTooltip title="可选标签可选标签可选标签可选标签" width={130}>
+                            可选标签可选标签可选标签可选标签
+                        </TextOverflowTooltip>
+                    )}
+                    key="1"
+                >
+                    Content of Tab Pane 1
+                </TabPane>
+                <TabPane tab="可选标签" key="2">
+                    Content of Tab Pane 2
+                </TabPane>
+                <TabPane tab="不可选标签" disabled key="3">
+                    Content of Tab Pane 3
+                </TabPane>
+            </Tabs>
+            <h3>带icon的tab样式</h3>
             <Tabs defaultActiveKey="1" style={{ marginBottom: 50 }}>
                 <TabPane
                     tab={
                         <span>
-                            <ForkOutlined />
+                            <IconHomeOutlined style={{ marginRight: 8 }} />
                             Tab 1
                         </span>
                     }
@@ -39,7 +77,7 @@ export const Demo = () => {
                 <TabPane
                     tab={
                         <span>
-                            <ForkOutlined />
+                            <IconHomeOutlined style={{ marginRight: 8 }} />
                             Tab 2
                         </span>
                     }
@@ -48,11 +86,13 @@ export const Demo = () => {
                     Tab 2
                 </TabPane>
             </Tabs>
+
+            <h3>带计数的tab样式</h3>
+
             <Tabs defaultActiveKey="1" style={{ marginBottom: 50 }}>
                 <TabPane
                     tab={
                         <span>
-                            <ForkOutlined />
                             Tab 1
                             <em className="num">15</em>
                         </span>
@@ -64,7 +104,6 @@ export const Demo = () => {
                 <TabPane
                     tab={
                         <span>
-                            <ForkOutlined />
                             Tab 2
                             <em className="num">15</em>
                         </span>
@@ -78,6 +117,149 @@ export const Demo = () => {
     );
 };
 
+export const ExtraDemo = () => {
+    const [active, setActive] = React.useState('更多');
+    const [activeContent, setActiveContent] = React.useState('Tab extra');
+    const reset = React.useCallback(
+        () => {
+            setActive('更多');
+            setActiveContent('Tab extra');
+        },
+        []
+    );
+    const handleSelect = React.useCallback(
+        item => {
+            setActive(item.key);
+            setActiveContent(item.key);
+        },
+        []
+    );
+    const handleTabChange = React.useCallback(
+        actvieKey => {
+            if (actvieKey === '1' || actvieKey === '2') {
+                reset();
+            }
+        },
+        [reset]
+    );
+    const menu = (
+        <Menu onClick={handleSelect}>
+            <Menu.Item key="选项三">
+                选项三
+            </Menu.Item>
+            <Menu.Item key="选项四">
+                选项四
+            </Menu.Item>
+            <Menu.Item key="选项五">
+                选项五
+            </Menu.Item>
+        </Menu>
+    );
+    const { TabPane } = Tabs;
+    return (
+        <>
+            <h3>tab收起，每两个tab间距是固定的，可根据具体情况将部分tab收起，或支持添加/自定义tab</h3>
+            <Tabs defaultActiveKey="1" style={{ marginBottom: 50 }} onChange={handleTabChange}>
+                <TabPane
+                    tab="选项一"
+                    key="1"
+                >
+                    选项一
+                </TabPane>
+                <TabPane
+                    tab="选项二"
+                    key="2"
+                >
+                    选项二
+                </TabPane>
+                <TabPane
+                    tab={
+                        <Dropdown overlay={menu}>
+                            <FlexCentered>
+                                {active}
+                                <IconDownOutlined style={{ paddingLeft: '5px' }} />
+                            </FlexCentered>
+                        </Dropdown>
+                    }
+                    key="3"
+                >
+                    {activeContent}
+                </TabPane>
+            </Tabs>
+        </>
+    );
+};
+
+export const AddDemo = () => {
+    const { TabPane } = Tabs;
+
+    const initialPanes = [
+        { title: 'Tab 1', content: 'Content of Tab 1', key: '1' },
+        { title: 'Tab 2', content: 'Content of Tab 2', key: '2' },
+        {
+            title: 'Tab 3',
+            content: 'Content of Tab 3',
+            key: '3',
+            closable: false,
+        },
+    ];
+
+    class Demo extends React.Component {
+        newTabIndex = 0;
+
+        state = {
+            activeKey: initialPanes[0].key,
+            panes: initialPanes,
+        };
+
+        onChange = activeKey => {
+            this.setState({ activeKey });
+        };
+
+        onEdit = (targetKey, action) => {
+            this[action](targetKey);
+        };
+
+        add = () => {
+            const { panes } = this.state;
+            const activeKey = `newTab${this.newTabIndex++}`;
+            const newPanes = [...panes];
+            newPanes.push({ title: 'New Tab', content: 'Content of new Tab', key: activeKey });
+            this.setState({
+                panes: newPanes,
+                activeKey,
+            });
+        };
+
+        render() {
+            const { panes, activeKey } = this.state;
+            return (
+                <Tabs
+                    onChange={this.onChange}
+                    activeKey={activeKey}
+                >
+                    {panes.map(pane => (
+                        <TabPane tab={pane.title} key={pane.key}>
+                            {pane.content}
+                        </TabPane>
+                    ))}
+                    <TabPane
+                        tab={<Button icon={<IconPlusOutlined />} onClick={this.add} />}
+                    >
+                        <></>
+                    </TabPane>
+                </Tabs>
+            );
+        }
+    }
+    return (
+        <>
+            <Blockquote>这个添加是TabPane改的，antd原带的add action和这个不同</Blockquote>
+            <h3>tab收起，每两个tab间距是固定的，可根据具体情况将部分tab收起，或支持添加/自定义tab</h3>
+            <Demo />
+        </>
+    );
+};
 
 export const PanelDemo = () => {
     const { TabPane } = Tabs;
