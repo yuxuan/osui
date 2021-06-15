7f4ad042882e6f2eaf5598c6555b98dbb18c64f1
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-06-15 14:04:29 +0800

    fix: 增加rate的调整
    Change-Id: I07081c63b4e6d007909ee21a7859fb0320045b82

--- packages/ui/rate/src/index.tsx
+++ packages/ui/rate/src/index.tsx
@@ -1,2 +1,20 @@
-import {Rate} from 'antd';
-export default Rate;
+import React from 'react';
+import {Rate as AntdRate} from 'antd';
+import {RateProps as AntdRateProps} from 'antd/lib/rate';
+import classNames from 'classnames';
+import hoistNonReactStatics from 'hoist-non-react-statics';
+import './index.less';
+
+const clsPrefix = 'osui-rate';
+
+const Rate: React.ForwardRefRenderFunction<any, AntdRateProps> = ({className, ...props}, ref) => {
+    const innerClassName = classNames(className, clsPrefix);
+    return <AntdRate ref={ref} className={innerClassName} {...props} />;
+};
+
+const RateRef = React.forwardRef<unknown, AntdRateProps>(Rate);
+
+hoistNonReactStatics(Rate, AntdRate);
+
+export default RateRef;
+
