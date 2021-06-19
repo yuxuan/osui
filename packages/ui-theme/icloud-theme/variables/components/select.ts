17e1194ad81405fd5f058a6d46f7672e42342f1c
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-06-19 11:48:18 +0800

    fix: 修复select箭头和清除按钮位置错位的问题
    Change-Id: I659ce318d73507df9afa7a2315c703c9ba11ce53

--- packages/ui-theme/icloud-theme/variables/components/select.ts
+++ packages/ui-theme/icloud-theme/variables/components/select.ts
@@ -6,7 +6,11 @@ export default {
     '--select-disabled-bg': 'var(--theme-disabled-bg)',
     '--select-check-icon-size': '12px',
     '--select-arrow-top': '14px', // 固定位置
+    '--select-arrow-top-small': '11px',
+    '--select-arrow-top-large': '19px',
     '--select-clear-icon-top': '16px',
+    '--select-clear-icon-top-small': '13px',
+    '--select-clear-icon-top-large': '20px',
     '--select-clear-icon-right': '30px',
     '--select-clear-icon-opacity': '0',
     '--select-clear-icon-opacity-hover': '1',
--- packages/ui/docs/icloud/stories/input/index.stories.tsx
+++ packages/ui/docs/icloud/stories/input/index.stories.tsx
@@ -33,44 +33,89 @@ export const Demo = () => {
         <>
             <BrandProvider brand="icloud">
                 <h3>基础输入框展示形式</h3>
-                <Input style={{ width: 360 }} placeholder="请输入" />
+                <Input
+                    style={{
+                        width: 360,
+                    }}
+                    placeholder="请输入"
+                />
                 <br />
                 <br />
                 <Input
                     disabled
-                    style={{ width: 360 }}
+                    style={{
+                        width: 360,
+                    }}
                     value="请输入产品线名称，这个是value的disable状态"
                 />
                 <br />
                 <br />
-                <TextArea style={{ width: 360, height: 150 }} placeholder="请输入" />
+                <TextArea
+                    style={{
+                        width: 360,
+                        height: 150,
+                    }}
+                    placeholder="请输入"
+                />
                 <br />
                 <br />
                 <p>密码输入框</p>
-                <Input.Password style={{ width: 360 }} placeholder="请输入" />
+                <Input.Password
+                    style={{
+                        width: 360,
+                    }}
+                    placeholder="请输入"
+                />
                 <br />
                 <br />
                 <p>清空按钮输入框</p>
-                <Input allowClear style={{ width: 360 }} placeholder="请输入" defaultValue="产品线名称" />
+                <Input
+                    allowClear
+                    style={{
+                        width: 360,
+                    }}
+                    placeholder="请输入"
+                    defaultValue="产品线名称"
+                />
                 <br />
                 <br />
                 <p>带有前后缀的输入框</p>
-                <Input prefix="￥" suffix="RMB" style={{ width: 360 }} />
+                <Input
+                    prefix="￥"
+                    suffix="RMB"
+                    style={{
+                        width: 360,
+                    }}
+                />
                 <br />
                 <br />
                 <Input.Group compact>
                     <Input
-                        style={{ width: '280px' }}
+                        style={{
+                            width: '280px',
+                        }}
                         placeholder="Email"
                     />
-                    <Select defaultValue="RMB" style={{ width: '80px' }}>
+                    <Select
+                        defaultValue="RMB"
+                        style={{
+                            width: '80px',
+                        }}
+                    >
                         <Select.Option value="RMB">RMB</Select.Option>
                         <Select.Option value="JPY">JPY</Select.Option>
                     </Select>
                 </Input.Group>
                 <br />
                 <br />
-                <Input.Search allowClear withSuffixIcon style={{ width: 360 }} placeholder="请输入" />
+                <Input.Search
+                    allowClear
+                    withSuffixIcon
+                    style={{
+                        width: 360,
+                    }}
+                    placeholder="请输入"
+                />
                 <p></p>
                 <p></p>
                 <h3>错误状态</h3>
@@ -123,16 +168,42 @@ export const Size = () => {
                 <p>宽度有三种： 160px，240px，320px。宽度没有给出默认设置，产品根据规范按需从这三个宽度选择合适的宽度</p>
             </Blockquote>
             <h3>高度</h3>
-            <Input style={{ width: 240 }} placeholder="small size" size="small" />
+            <Input
+                style={{
+                    width: 240,
+                }}
+                placeholder="small size"
+                size="small"
+            />
             <p />
-            <Input style={{ width: 240 }} placeholder="default size" />
+            <Input
+                style={{
+                    width: 240,
+                }}
+                placeholder="default size"
+            />
             <p></p>
             <h3>宽度</h3>
-            <Input style={{ width: 160 }} placeholder="请输入" />
+            <Input
+                style={{
+                    width: 160,
+                }}
+                placeholder="请输入"
+            />
             <p />
-            <Input style={{ width: 240 }} placeholder="请输入" />
+            <Input
+                style={{
+                    width: 240,
+                }}
+                placeholder="请输入"
+            />
             <p />
-            <Input style={{ width: 320 }} placeholder="请输入" />
+            <Input
+                style={{
+                    width: 320,
+                }}
+                placeholder="请输入"
+            />
         </>
     );
 };
@@ -195,12 +266,19 @@ export const Complete = () => {
                 上面是使用addonBefore，和addonAfter来实现的，而下面是Input.Group的方式
             </Blockquote>
             <Input.Group compact>
-                <Select defaultValue="Sign Up" style={{ width: '80px' }}>
+                <Select
+                    defaultValue="Sign Up"
+                    style={{
+                        width: '80px',
+                    }}
+                >
                     <Option value="Sign Up">Sign Up</Option>
                     <Option value="Sign In">Sign In</Option>
                 </Select>
                 <AutoComplete
-                    style={{ width: '70%' }}
+                    style={{
+                        width: '70%',
+                    }}
                     placeholder="Email"
                     options={[{ value: 'text 1' }, { value: 'text 2' }]}
                 />
@@ -242,7 +320,13 @@ export const Complete = () => {
                     extra="由大小写字母、数字组成，必须以字母开头"
                     help={'格式错误，请按提示规则录入'}
                 >
-                    <Input.Search withSuffixIcon placeholder="input search text" style={{ width: 240 }} />
+                    <Input.Search
+                        withSuffixIcon
+                        placeholder="input search text"
+                        style={{
+                            width: 240,
+                        }}
+                    />
                 </Form.Item>
                 <Form.Item
                     validateMessageLayout="inline"
@@ -252,12 +336,19 @@ export const Complete = () => {
                     help={'格式错误，请按提示规则录入'}
                 >
                     <Input.Group compact>
-                        <Select defaultValue="Sign Up" style={{ width: '80px' }}>
+                        <Select
+                            defaultValue="Sign Up"
+                            style={{
+                                width: '80px',
+                            }}
+                        >
                             <Option value="Sign Up">Sign Up</Option>
                             <Option value="Sign In">Sign In</Option>
                         </Select>
                         <AutoComplete
-                            style={{ width: '70%' }}
+                            style={{
+                                width: '70%',
+                            }}
                             placeholder="Email"
                             options={[{ value: 'text 1' }, { value: 'text 2' }]}
                         />
@@ -281,14 +372,18 @@ export const Search = () => {
                         allowClear
                         withSuffixIcon
                         placeholder="搜索提示文案"
-                        style={{ width: 240 }}
+                        style={{
+                            width: 240,
+                        }}
                     />
                     <Gap orientation="horizontal" factor={6} base={4} />
                     <Input.Search
                         allowClear
                         enterButton="搜索"
                         placeholder="搜索提示文案"
-                        style={{ width: 240 }}
+                        style={{
+                            width: 240,
+                        }}
                     />
                 </FlexCentered>
                 <br />
@@ -297,20 +392,38 @@ export const Search = () => {
                         disabled
                         withSuffixIcon
                         placeholder="搜索提示文案"
-                        style={{ width: 240 }}
+                        style={{
+                            width: 240,
+                        }}
                     />
                     <Gap orientation="horizontal" factor={6} base={4} />
                     <Input.Search
                         disabled
                         enterButton={<Button disabled type="primary">搜索</Button>}
                         placeholder="搜索提示文案"
-                        style={{ width: 240 }}
+                        style={{
+                            width: 240,
+                        }}
+                    />
+                    <Gap orientation="horizontal" factor={6} base={4} />
+                    <Input.Search
+                        loading
+                        enterButton="搜索"
+                        placeholder="搜索提示文案"
+                        style={{
+                            width: 240,
+                        }}
                     />
                 </FlexCentered>
                 <Blockquote>
                     下面第一个这种不在规范内，不要使用，如果要用，加上 withSuffixIcon
                 </Blockquote>
-                <Input.Search placeholder="搜索提示文案" style={{ width: 240 }} />
+                <Input.Search
+                    placeholder="搜索提示文案"
+                    style={{
+                        width: 240,
+                    }}
+                />
             </BrandProvider>
         </>
     );
