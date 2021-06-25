102c8c36656423ac8795fe6645a1ef99516f2f06
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-06-25 16:57:31 +0800

    fix: select样是调整
    Change-Id: I9ec2a370cf46ec461fdd3ad5539f7b100afb657d

--- packages/ui/select/stories/icloud.stories.tsx
+++ packages/ui/select/stories/icloud.stories.tsx
@@ -18,7 +18,7 @@ export const Demo = () => {
             <BrandProvider brand="icloud">
                 <h2>基础下拉</h2>
                 <p>placeholder</p>
-                <Select placeholder="请选择" style={{ width: 240 }} onChange={handleChange}>
+                <Select placeholder="请选择" style={{ width: 240, }} onChange={handleChange}>
                     <Option value="jack">Jack</Option>
                     <Option value="lucy">Lucy</Option>
                     <Option value="disabled" disabled>
@@ -29,7 +29,7 @@ export const Demo = () => {
                 <br />
                 <br />
                 <p>有默认值</p>
-                <Select defaultValue="lucy" style={{ width: 240 }} onChange={handleChange}>
+                <Select defaultValue="lucy" style={{ width: 240, }} onChange={handleChange}>
                     <Option value="jack">Jack</Option>
                     <Option value="lucy">Lucy</Option>
                     <Option value="disabled" disabled>
@@ -40,25 +40,25 @@ export const Demo = () => {
                 <br />
                 <br />
                 <p>disabled</p>
-                <Select defaultValue="lucy" style={{ width: 240 }} disabled>
+                <Select defaultValue="lucy" style={{ width: 240, }} disabled>
                     <Option value="lucy">Lucy</Option>
                 </Select>
                 <br />
                 <br />
                 <p>loading</p>
-                <Select defaultValue="lucy" style={{ width: 240 }} loading>
+                <Select defaultValue="lucy" style={{ width: 240, }} loading>
                     <Option value="lucy">Lucy</Option>
                 </Select>
                 <br />
                 <br />
                 <p>无边框样式</p>
-                <Select defaultValue="lucy" style={{ width: 240 }} noBorder>
+                <Select defaultValue="lucy" style={{ width: 240, }} noBorder>
                     <Option value="lucy">Lucy</Option>
                 </Select>
                 <br />
                 <br />
                 <p>内容为空</p>
-                <Select defaultValue="lucy" style={{ width: 240 }} />
+                <Select defaultValue="lucy" style={{ width: 240, }} />
             </BrandProvider>
         </>
     );
@@ -73,10 +73,10 @@ export const Size = () => {
 
     return (
         <Space>
-            <Select placeholder="small size" style={{ width: 240 }} size="small">
+            <Select placeholder="small size" style={{ width: 240, }} size="small">
                 <Option value="lucy">Lucy</Option>
             </Select>
-            <Select placeholder="default size" style={{ width: 240 }} onChange={handleChange}>
+            <Select placeholder="default size" style={{ width: 240, }} onChange={handleChange}>
                 <Option value="jack">Jack</Option>
                 <Option value="lucy">Lucy</Option>
                 <Option value="disabled" disabled>
@@ -84,7 +84,7 @@ export const Size = () => {
                 </Option>
                 <Option value="Yiminghe">yiminghe</Option>
             </Select>
-            <Select placeholder="large size" style={{ width: 240 }} onChange={handleChange} size="large">
+            <Select placeholder="large size" style={{ width: 240, }} onChange={handleChange} size="large">
                 <Option value="jack">Jack</Option>
                 <Option value="lucy">Lucy</Option>
                 <Option value="disabled" disabled>
@@ -107,7 +107,7 @@ export const MultipleDemo = () => {
             <BrandProvider>
                 <h2>无限制下拉多选</h2>
                 <p>默认多选使用方式：</p>
-                <Select mode="multiple" placeholder="请选择" style={{ width: 240 }} onChange={handleChange}>
+                <Select mode="multiple" placeholder="请选择" style={{ width: 240, }} onChange={handleChange}>
                     <Option value="jack">Jack</Option>
                     <Option value="lucy">Lucy</Option>
                     <Option value="disabled" disabled>
@@ -122,7 +122,7 @@ export const MultipleDemo = () => {
                     allowClear={false}
                     mode="multiple"
                     defaultValue="lucy"
-                    style={{ width: 240 }}
+                    style={{ width: 240, }}
                     onChange={handleChange}
                 >
                     <Option value="jack">Jack</Option>
@@ -142,7 +142,7 @@ export const MultipleDemo = () => {
                     allowClear
                     mode="multiple"
                     defaultValue="lucy"
-                    style={{ width: 240 }}
+                    style={{ width: 240, }}
                     onChange={handleChange}
                 >
                     <Option value="jack">Jack</Option>
@@ -175,3 +175,20 @@ export const Api = () => {
     );
 };
 
+export const TestCase = () => {
+    const Option = Select.Option;
+    return (
+        <>
+            <p>最短的时候</p>
+            <Select defaultValue="lucy" >
+                <Option value="jack">Jack</Option>
+                <Option value="lucy">Lucy</Option>
+                <Option value="disabled" disabled>
+                    Disabled
+                </Option>
+                <Option value="Yiminghe">yiminghe</Option>
+            </Select>
+        </>
+    );
+
+};
