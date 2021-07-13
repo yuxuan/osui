f379497a9cd8580c6f37cedb14323a87f89d4cec
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-07-13 16:58:31 +0800

    fix: 面包屑展示前两个item
    Change-Id: I317ff7b5a0cc339c547dca64c8c6ae8b2aea7b8f

--- packages/ui/breadcrumb/src/index.tsx
+++ packages/ui/breadcrumb/src/index.tsx
@@ -1,55 +1,65 @@
-import React, { useReducer } from 'react';
-import { Breadcrumb as AntdBreadcrumb } from 'antd';
-import { BreadcrumbProps as AntdBrandcrumbProps } from 'antd/lib/breadcrumb';
+import React, {useReducer} from 'react';
+import {Breadcrumb as AntdBreadcrumb} from 'antd';
+import {BreadcrumbProps as AntdBrandcrumbProps} from 'antd/lib/breadcrumb';
 import classNames from 'classnames';
+
 import './index.less';
 
 const clsPrefix = 'osui-breadcrumb';
 
 const Ellipsis = ({onClick}: {onClick: () => void}) => (
-    <span className={`${clsPrefix}-ellipsis`} onClick={onClick}>...</span>
+    <span className={`${clsPrefix}-ellipsis`} onClick={onClick}>
+        ...
+    </span>
 );
 
 interface EllipsisConfig {
     shownLastItems: number; // 已经展示出来的最后的items数
     onEllipsisClick: () => void;
     maxItemLength: number;
+    headItemLength: number;
+    tailItemLength: number;
 }
 type GetItemRenderWithEllipsis = (
     render: AntdBrandcrumbProps['itemRender'],
-    config: EllipsisConfig
+    config: EllipsisConfig,
 ) => AntdBrandcrumbProps['itemRender'];
 
 const getItemRenderWithEllipsis: GetItemRenderWithEllipsis = (render, config) => (route, params, routes, paths) => {
-    const {shownLastItems, onEllipsisClick} = config;
+    const {shownLastItems, onEllipsisClick, headItemLength} = config;
 
     const index = routes.indexOf(route);
-    // 当展示第一个item 且 总长度 - 展示出来后n个 > 1 时，展示 ...
-    if (index === 1 && routes.length - shownLastItems > 1) {
+    // 当展示前两个item 且 总长度 - 展示出来后n个 > 2 时，展示 ...
+    if (index === headItemLength && routes.length - shownLastItems > headItemLength) {
         return <Ellipsis onClick={onEllipsisClick} />;
     }
-    // 隐藏掉 1 到 展示出来后n个之间的 item
-    if (index > 1 && index < routes.length - shownLastItems) {
+    // 隐藏掉 2 到 展示出来后n个之间的 item
+    if (index > headItemLength && index < routes.length - shownLastItems) {
         return null;
     }
 
     return render && render(route, params, routes, paths);
 };
 
-const getChildrenWithEllipsis = (children: React.ReactNode | React.ReactNode[], config: EllipsisConfig) => {
-    const {shownLastItems, onEllipsisClick, maxItemLength} = config;
+const getChildrenWithEllipsis = (
+    children: React.ReactNode | React.ReactNode[],
+    config: EllipsisConfig
+) => {
+    const {shownLastItems, onEllipsisClick, maxItemLength, headItemLength} = config;
     // 如果不是array，或者children长度小于maxItemLenght时，直接展示children
     if (!Array.isArray(children) || children.length <= maxItemLength) {
         return children;
     }
-    // 如果剩余最后一个item没展示时，去掉Ellipsis，展示所有
-    if (children.length - shownLastItems <= 1) {
+    // 如果剩余最后两个item没展示时，去掉Ellipsis，展示所有
+    if (children.length - shownLastItems <= headItemLength) {
         return children;
     }
-    // 展示 第一个 ... 倒数两个
+    // 展示 第一个，第二个， ... 倒数两个
     return [
-        children[0],
-        <AntdBreadcrumb.Item key="_ellipsis"><Ellipsis onClick={onEllipsisClick} /></AntdBreadcrumb.Item>,
+        ...children.slice(0, headItemLength),
+        <AntdBreadcrumb.Item key="_ellipsis">
+            <Ellipsis onClick={onEllipsisClick} />
+        </AntdBreadcrumb.Item>,
         ...children.slice(-shownLastItems),
     ];
 };
@@ -63,18 +73,37 @@ export interface BreadcrumbProps extends AntdBrandcrumbProps {
      * @description 配合showEllipsis使用，当showEllipsis为true时，超过这个数字则会展示 ...
      */
     maxItemLength?: number;
+    /**
+     * @description 面包屑过长时头部items个数 ...
+     */
+    headItemLength?: number;
+    /**
+     * @description 面包屑过长时尾部items个数 ...
+     */
+    tailItemLength?: number;
 }
 
-export interface BreadcrumbDecorator extends React.FC<BreadcrumbProps> {
+export interface BreadcrumbInterface extends React.FC<BreadcrumbProps> {
     Item: typeof AntdBreadcrumb.Item;
     Separator: typeof AntdBreadcrumb.Separator;
 }
 
-const Breadcrumb: BreadcrumbDecorator = props => {
-    const {children, routes, itemRender, showEllipsis, className, maxItemLength = 5, ...restProps} = props;
+const Breadcrumb: BreadcrumbInterface = props => {
+    const {
+        children,
+        routes,
+        itemRender,
+        showEllipsis,
+        className,
+        maxItemLength = 5,
+        headItemLength = 2,
+        tailItemLength = 2,
+        ...restProps
+    } = props;
 
     const [shownLastItems, doPop] = useReducer<React.Reducer<number, void>>(
         (index: number) => {
+            // step
             if (children && Array.isArray(children) && children.length - index > 1) {
                 return index + 1;
             }
@@ -83,7 +112,7 @@ const Breadcrumb: BreadcrumbDecorator = props => {
             }
             return 0;
         },
-        2
+        tailItemLength
     );
 
     const isShowEllipsis = showEllipsis
@@ -92,20 +121,22 @@ const Breadcrumb: BreadcrumbDecorator = props => {
             || (children && Array.isArray(children) && children.length > maxItemLength)
         );
 
-    const config = {shownLastItems, onEllipsisClick: doPop, maxItemLength};
+    const config = {shownLastItems, onEllipsisClick: doPop, maxItemLength, headItemLength, tailItemLength};
 
     return (
         <AntdBreadcrumb
             className={classNames(clsPrefix, className)}
             itemRender={
-                (isShowEllipsis && itemRender)
+                isShowEllipsis && itemRender
                     ? getItemRenderWithEllipsis(itemRender, config)
                     : itemRender
             }
             routes={routes}
             {...restProps}
         >
-            {(isShowEllipsis && children) ? getChildrenWithEllipsis(children, config) : children}
+            {isShowEllipsis && children
+                ? getChildrenWithEllipsis(children, config)
+                : children}
         </AntdBreadcrumb>
     );
 };
