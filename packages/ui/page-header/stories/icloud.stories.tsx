0df8cf3ef51e0745dbb172f548b69f25a0dd118b
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-09-07 14:38:54 +0800

    fix: 取消对page-header的调整
    Change-Id: I7ada55c47e35c04d5cde51e889a2608f3b7c5ddd

--- packages/ui/page-header/stories/icloud.stories.tsx
+++ packages/ui/page-header/stories/icloud.stories.tsx
@@ -1,8 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import {IconInfoCircleFilled} from '@osui/icons-icloud';
-import Gap from '@osui/gap';
-import Button from '@osui/button';
 import PageHeader from '../src';
 
 export default {
@@ -13,33 +10,7 @@ export default {
 export const Demo = () => {
     return (
         <>
-            <h3>一、基础页头</h3>
-            <PageHeader
-                title="标题标题标题标题标题标题标题标题"
-                subTitle="补充备注补充备注补充备注"
-                extra={
-                    <div style={{display: 'flex', alignItems: 'center', color: 'var(--color-gray-7)'}}>
-                        <IconInfoCircleFilled /><Gap orientation="horizontal" factor={1} />帮助文档
-                    </div>
-                }
-            />
-            <p></p>
-            <p></p>
-            <p></p>
-            <PageHeader
-                title="标题"
-                subTitle="按使用流量计费"
-                extra={
-                    <>
-                        <Button>价格计算器</Button>
-                    </>
-                }
-            />
-            <p></p>
-            <p></p>
-            <p></p>
-            <p>noPadding: padding可以根据排版需要自行设置</p>
-            <PageHeader noPadding title="Title" />
+            <p>请参考业务组件库icloud-ui</p>
         </>
     );
 };
