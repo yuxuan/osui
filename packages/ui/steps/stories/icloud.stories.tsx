29aee17b9e29c210bcbdcdb3bfd8d2d771b4cf2e
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-09-13 13:33:12 +0800

    fix: steps调整
    Change-Id: I42ea99682890e701497bfed2873e5675db16b045

--- packages/ui/steps/stories/icloud.stories.tsx
+++ packages/ui/steps/stories/icloud.stories.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React, { useState } from 'react';
 import Button from '@osui/button';
 import Space from '@osui/space';
@@ -11,18 +12,86 @@ export const Demo = () => {
     const { Step } = Steps;
     const { Step: ProcessOnlyStep } = Steps.ProcessOnlySteps;
     const [current, setCurrent] = useState(-1);
+    const onChange = current => {
+        setCurrent(current);
+    };
     return (
         <>
-            <p>点击底部下一步上一步按钮，可以查看完成状态</p>
-            <p>以下为带完成状态的Steps，UE规范中没有，不建议使用</p>
-            <Steps current={current}>
-                <Step title="Step 1" />
-                <Step title="Step 2" />
-                <Step title="Step 3" />
+            <h2>纵向步骤条</h2>
+            <Steps direction="vertical" current={1}>
+                <Step title="已完成步骤" description="描述文案" status="finish" />
+                <Step title="当前步骤" description="描述文案" />
+                <Step title="出错步骤" description="描述文案" status="error" />
+                <Step title="未完成步骤" description="描述文案" />
             </Steps>
-            <br />
-            <br />
-            <p>以下为ProcessOnlyStepSteps， 注意：需要使用Steps.ProcessOnlySteps</p>
+            <p>以下当前错误状态的展示</p>
+            <Steps direction="vertical" current={0}>
+                <Step title="当前错误" description="描述文案" status="error" />
+            </Steps>
+            <p>可点击切换</p>
+            <Steps direction="vertical" current={current} onChange={onChange}>
+                <Step title="已完成步骤" description="描述文案" status="finish" />
+                <Step title="当前步骤" description="描述文案" />
+                <Step title="出错步骤" description="描述文案" status="error" />
+                <Step title="未完成步骤" description="描述文案" />
+            </Steps>
+            <p></p>
+            <p></p>
+            <h2>横向步骤条</h2>
+            <Steps current={2} >
+                <Step title="提交申请" description="描述文案" status="finish" />
+                <Step title="产品线分组审批人审批" description="描述文案" status="finish" />
+                <Step title="search-cost审核" description="描述文案" />
+                <Step title="SRE资源效率审核" />
+                <Step title="资源交付" />
+            </Steps>
+            <p></p>
+            <p></p>
+            <Steps current={2} labelPlacement="vertical">
+                <Step title="提交申请" description="描述文案" status="finish" />
+                <Step title="步骤名称" description="描述文案" status="finish" />
+                <Step title="当前步骤" description="描述文案" />
+                <Step title="步骤名称" />
+                <Step title="步骤名称" />
+            </Steps>
+            <p></p>
+            <p></p>
+            <h2>其它样式</h2>
+            <p>可点击切换</p>
+            <Steps current={current} onChange={onChange} >
+                <Step title="已完成步骤" description="描述文案" status="finish" />
+                <Step title="当前步骤" description="描述文案" />
+                <Step title="出错步骤" description="描述文案" status="error" />
+                <Step title="未完成步骤" />
+            </Steps>
+            <p></p>
+            <p></p>
+            <h2>点线型步骤条</h2>
+            <Steps progressDot current={1}>
+                <Step title="已完成步骤" description="这是一段描述性的文字" />
+                <Step title="当前步骤" description="这是一段描述性的文字" />
+                <Step title="未完成步骤" description="这是一段描述性的文字" />
+            </Steps>
+            <p></p>
+            <p></p>
+            <Steps progressDot current={1}>
+                <Step title="已完成步骤" description="这是一段描述性的文字" />
+                <Step title="当前步骤" description="这是一段描述性的文字" status="error" />
+                <Step title="未完成步骤" description="这是一段描述性的文字" />
+            </Steps>
+            <p></p>
+            <p></p>
+            <h2>纵向</h2>
+            <Steps progressDot current={1} direction="vertical">
+                <Step title="已完成步骤" description="这是一段描述性的文字" />
+                <Step title="当前步骤" description="这是一段描述性的文字" status="error" />
+                <Step title="未完成步骤" description="这是一段描述性的文字" />
+            </Steps>
+            <p></p>
+            <p></p>
+            <h2>其它</h2>
+
+            <p>以下为ProcessOnlyStepSteps， 注意：需要使用Steps.ProcessOnlySteps，只会显示process的状态</p>
             <Steps.ProcessOnlySteps current={current}>
                 <ProcessOnlyStep title="Step 1" />
                 <ProcessOnlyStep title="Step 2" />
@@ -40,33 +109,6 @@ export const Demo = () => {
             </Steps.ProcessOnlySteps>
             <br />
             <br />
-            <p>以下为垂直形式</p>
-            <Steps.ProcessOnlySteps current={current} direction="vertical">
-                <ProcessOnlyStep title="Step 1" description="This is a description." />
-                <ProcessOnlyStep title="Step 2" description="This is a description." />
-                <ProcessOnlyStep title="Step 3" description="This is a description." />
-            </Steps.ProcessOnlySteps>
-            <Space>
-                <Button onClick={() => setCurrent(step => step + 1)}>下一步</Button>
-                <Button onClick={() => setCurrent(step => step - 1)}>上一步</Button>
-            </Space>
-        </>
-    );
-};
-
-export const ErrorStatusDemo = () => {
-    const ProcessOnlySteps = Steps.ProcessOnlySteps;
-    const { Step } = ProcessOnlySteps;
-    const [current, setCurrent] = useState(-1);
-    return (
-        <>
-            <ProcessOnlySteps current={current}>
-                <Step title="Step 1" />
-                <Step title="Step 2" />
-                <Step title="Step 3" status="error" />
-            </ProcessOnlySteps>
-            <br />
-            <br />
             <Space>
                 <Button onClick={() => setCurrent(step => step + 1)}>下一步</Button>
                 <Button onClick={() => setCurrent(step => step - 1)}>上一步</Button>
@@ -80,7 +122,7 @@ export const CompactDemo = () => {
     const [current, setCurrent] = useState(-1);
     return (
         <>
-            <h3>紧凑型</h3>
+            <h2>紧凑型</h2>
             <p>当需要展示很多步时</p>
             <Steps.ProcessOnlySteps current={current} compact type="navigation">
                 <ProcessOnlyStep title="Step 1" />
