4c71b2c98cd12a1de4cf08f7a9fa1157ffdbc74d
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2021-09-29 17:09:39 +0800

    fix: comments调整
    Change-Id: I3d691990d44187606fd11393bf7953232b4c37e6

--- packages/ui/comment/src/index.tsx
+++ packages/ui/comment/src/index.tsx
@@ -1,3 +1,18 @@
-import {Comment} from 'antd';
-export type { CommentProps } from 'antd';
-export default Comment;
+import React from 'react';
+import {Comment as AntdComment} from 'antd';
+import type {CommentProps} from 'antd';
+import classNames from 'classnames';
+import hoistNonReactStatics from 'hoist-non-react-statics';
+import './index.less';
+
+const clsPrefix = 'osui-comment';
+
+const OSUIComment = ({className, ...props}: CommentProps) => {
+    const innerClassNames = classNames(clsPrefix, className);
+    return (<AntdComment className={innerClassNames} {...props} />);
+};
+
+hoistNonReactStatics(OSUIComment, AntdComment);
+
+export type {CommentProps};
+export default OSUIComment;
