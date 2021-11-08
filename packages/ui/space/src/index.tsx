5a1d80fec4b727cdda7e8ae733629baaccb3e4ed
ngdangku <9939629+ngdangku@user.noreply.gitee.com>
2021-11-08 14:00:35 +0800

    fix: space组件的size默认值优先从GlobalConfig取

--- packages/ui/space/src/index.tsx
+++ packages/ui/space/src/index.tsx
@@ -1,13 +1,16 @@
-import React, {FC} from 'react';
 import {Space as AntdSpace} from 'antd';
+import {ConfigContext} from 'antd/lib/config-provider';
 import {SpaceProps} from 'antd/lib/space';
 import useFlexGapSupport from 'antd/lib/_util/hooks/useFlexGapSupport';
 import classNames from 'classnames';
+import React, {FC, useContext} from 'react';
 import './index.less';
 
 const clsPrefix = 'osui-space';
 
-const Space: FC<SpaceProps> = ({className, size = 'small', ...props}) => {
+const Space: FC<SpaceProps> = ({className, ...props}) => {
+    const {space} = useContext(ConfigContext);
+    const size = props.size || space?.size || 'small';
     const supportFlexGap = useFlexGapSupport();
     const innerClassName = classNames(
         clsPrefix,
@@ -20,5 +23,5 @@ const Space: FC<SpaceProps> = ({className, size = 'small', ...props}) => {
     );
 };
 
-export type { SpaceProps } from 'antd';
+export type {SpaceProps} from 'antd';
 export default Space;
