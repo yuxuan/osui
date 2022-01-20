5c546290594dc2d991c98c076c2dc82d04fd466a
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2022-01-20 13:17:09 +0800

    docs: 更新select的示例
    Change-Id: Id2f1e5c90c3a56966c8057ec55b7174d6d1c659d

--- packages/ui/select/stories/icloud.stories.tsx
+++ packages/ui/select/stories/icloud.stories.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import Space from '@osui/space';
 import BrandProvider from '@osui/brand-provider';
+import {Popover, Tag} from '@osui/ui';
 import Select from '../src';
 
 export default {
@@ -196,6 +197,82 @@ export const MultipleDisplayInPopup = () => {
                     <Option value="c">c</Option>
                 </Select>
             </BrandProvider>
+        </>
+    );
+};
+
+export const MulipleDisplayInPopupByHand = () => {
+    const Option = Select.Option;
+    const [selectedValue, setSelectedValue] = useState<string[]>([]);
+    const handleChange = useCallback(
+        value => {
+            setSelectedValue(value);
+        },
+        [setSelectedValue]
+    );
+    const handleClose = useCallback(
+        tag => {
+            const newSelectedValue = selectedValue.filter(v => v !== tag.value);
+            setSelectedValue(newSelectedValue);
+        },
+        [selectedValue, setSelectedValue]
+    );
+    const renderMaxTagPlaceholder = useCallback(
+        args => {
+            if (!args.length) {
+                return null;
+            }
+            const TagContent = (
+                // 点击关闭时不要触发select open
+                <div onMouseDown={e => e.stopPropagation()} onClick={e => e.stopPropagation()}>
+                    {
+                        args.map(
+                            (tag: any) => (
+                                <Tag
+                                    key={tag.key ?? tag.value}
+                                    closable
+                                    // eslint-disable-next-line react/jsx-no-bind
+                                    onClose={() => {
+                                        handleClose(tag);
+                                    }}
+                                >
+                                    {tag.value}
+                                </Tag>
+                            )
+                        )
+                    }
+                </div>
+            );
+            return (
+                <Popover content={TagContent}>
+                    {`${args.length}+...`}
+                </Popover>
+            );
+        },
+        [handleClose]
+    );
+    return (
+        <>
+            <p>不适用属性，直接手动实现方式</p>
+            <Select
+                allowClear
+                mode="multiple"
+                style={{width: 240}}
+                value={selectedValue}
+                onChange={handleChange}
+                maxTagCount="responsive"
+                maxTagPlaceholder={renderMaxTagPlaceholder}
+            >
+                <Option value="jack">Jack</Option>
+                <Option value="lucy">Lucy</Option>
+                <Option value="disabled" disabled>
+                    Disabled
+                </Option>
+                <Option value="Yiminghe">yiminghe</Option>
+                <Option value="a">a</Option>
+                <Option value="b">b</Option>
+                <Option value="c">c</Option>
+            </Select>
         </>
     );
 };
