44f305b2139595258a59dec3f709aeb529427eb0
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2022-03-07 16:09:50 +0800

    fix: tag调整
    Change-Id: I76225b00dfa9095a07e6276c27c337e249f140c3

--- packages/ui/tag/stories/icloud.stories.tsx
+++ packages/ui/tag/stories/icloud.stories.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import {Divider} from 'antd';
+import {Divider, Input, Tooltip} from '@osui/ui';
+import {PlusOutlined} from '@ant-design/icons';
+
 import {IconPlusOutlined} from '@osui/icons-icloud';
 import ClockCircleOutlined from '@ant-design/icons/ClockCircleOutlined';
 import Tag from '../src';
@@ -55,6 +57,145 @@ export const Demo = () => {
             <Tag color="purple" round>标签</Tag>
         </>
     );
+};
+
+export const Add = () => {
+    class EditableTagGroup extends React.Component {
+        state = {
+            tags: ['Unremovable', 'Tag 2', 'Tag 3'],
+            inputVisible: false,
+            inputValue: '',
+            editInputIndex: -1,
+            editInputValue: '',
+        };
+
+        handleClose = removedTag => {
+            const tags = this.state.tags.filter(tag => tag !== removedTag);
+            console.log(tags);
+            this.setState({tags});
+        };
+
+        showInput = () => {
+            this.setState({inputVisible: true}, () => this.input.focus());
+        };
+
+        handleInputChange = e => {
+            this.setState({inputValue: e.target.value});
+        };
+
+        handleInputConfirm = () => {
+            const {inputValue} = this.state;
+            let {tags} = this.state;
+            if (inputValue && !tags.includes(inputValue)) {
+                tags = [...tags, inputValue];
+            }
+            console.log(tags);
+            this.setState({
+                tags,
+                inputVisible: false,
+                inputValue: '',
+            });
+        };
+
+        handleEditInputChange = e => {
+            this.setState({editInputValue: e.target.value});
+        };
+
+        handleEditInputConfirm = () => {
+            this.setState(({tags, editInputIndex, editInputValue}) => {
+                const newTags = [...tags];
+                newTags[editInputIndex] = editInputValue;
+
+                return {
+                    tags: newTags,
+                    editInputIndex: -1,
+                    editInputValue: '',
+                };
+            });
+        };
+
+        saveInputRef = input => {
+            this.input = input;
+        };
+
+        saveEditInputRef = input => {
+            this.editInput = input;
+        };
+
+        render() {
+            const {tags, inputVisible, inputValue, editInputIndex, editInputValue} = this.state;
+            return (
+                <>
+                    {tags.map((tag, index) => {
+                        if (editInputIndex === index) {
+                            return (
+                                <Input
+                                    ref={this.saveEditInputRef}
+                                    key={tag}
+                                    size="small"
+                                    className="tag-input"
+                                    value={editInputValue}
+                                    onChange={this.handleEditInputChange}
+                                    onBlur={this.handleEditInputConfirm}
+                                    onPressEnter={this.handleEditInputConfirm}
+                                />
+                            );
+                        }
+
+                        const isLongTag = tag.length > 20;
+
+                        const tagElem = (
+                            <Tag
+                                className="edit-tag"
+                                key={tag}
+                                closable={index !== 0}
+                                onClose={() => this.handleClose(tag)}
+                            >
+                                <span
+                                    onDoubleClick={e => {
+                                        if (index !== 0) {
+                                            this.setState({editInputIndex: index, editInputValue: tag}, () => {
+                                                this.editInput.focus();
+                                            });
+                                            e.preventDefault();
+                                        }
+                                    }}
+                                >
+                                    {isLongTag ? `${tag.slice(0, 20)}...` : tag}
+                                </span>
+                            </Tag>
+                        );
+                        return isLongTag ? (
+                            <Tooltip title={tag} key={tag}>
+                                {tagElem}
+                            </Tooltip>
+                        ) : (
+                            tagElem
+                        );
+                    })}
+                    {inputVisible && (
+                        <Input
+                            ref={this.saveInputRef}
+                            type="text"
+                            size="small"
+                            className="tag-input"
+                            value={inputValue}
+                            onChange={this.handleInputChange}
+                            onBlur={this.handleInputConfirm}
+                            onPressEnter={this.handleInputConfirm}
+                        />
+                    )}
+                    {!inputVisible && (
+                        <Tag className="site-tag-plus" onClick={this.showInput}>
+                            <PlusOutlined /> New Tag
+                        </Tag>
+                    )}
+                </>
+            );
+        }
+    }
+
+    return <EditableTagGroup />;
 };
 
 export const Api = () => {
