2ae07df29b73ccc2b674c8e8abf7101539e615b3
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2022-03-08 14:29:52 +0800

    dev: 升级storybook
    Change-Id: Ice075515e79dac8a8d195f30fd96d806c10ada5d

--- packages/storybookbase/main.js
+++ packages/storybookbase/main.js
@@ -86,6 +86,14 @@ module.exports = () => ({
         config.resolve.extensions.push('.ts', '.tsx');
         config.resolve.alias['@'] = path.resolve(__dirname, '../src');
         config.resolve.alias['@osui/icons'] = '@osui/icons-icloud';
+
+        config.module.rules[0].use[0].options.presets = [
+            require.resolve('@babel/preset-react'),
+            require.resolve('@babel/preset-env'),
+            require.resolve('@emotion/babel-preset-css-prop'),
+        ];
+
+
         return config;
     },
 });
