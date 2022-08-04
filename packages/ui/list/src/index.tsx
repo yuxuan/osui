498c6424f02033b4f6012c006f659e45e19cea68
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2022-08-04 19:50:00 +0800

    fix: List修复loading

--- packages/ui/list/src/index.tsx
+++ packages/ui/list/src/index.tsx
@@ -1,3 +1,21 @@
-import {List} from 'antd';
-export type { ListProps } from 'antd';
-export default List;
+import React from 'react';
+import Spin from '@osui/spin';
+import {List as AntdList, ListProps as AntdListProps} from 'antd';
+import hoistNonReactStatics from 'hoist-non-react-statics';
+
+function List<T>(props: React.PropsWithChildren<AntdListProps<T>>) {
+    const {loading} = props;
+    const innerLoading = typeof loading === 'object' ? {
+        indicator: <Spin />,
+        ...loading,
+    } : {
+        indicator: <Spin />,
+        loading,
+    };
+    return <AntdList {...props} loading={innerLoading} />;
+}
+
+hoistNonReactStatics(List, AntdList);
+
+export type {ListProps} from 'antd';
+export default List as typeof AntdList;
