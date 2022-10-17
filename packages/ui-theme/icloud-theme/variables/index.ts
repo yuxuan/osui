3b832764395d324d116906f2106ab4380c63ba5b
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2022-10-17 15:38:01 +0800

    fix: 修复icloud-theme 构建出错

--- packages/ui-theme/icloud-theme/variables/index.ts
+++ packages/ui-theme/icloud-theme/variables/index.ts
@@ -1,13 +1,10 @@
 import v1 from './v1';
-import v2 from './v2';
 import acud from './acud';
 import dark from './dark';
 
 export {
     v1,
-    v2,
     dark,
     acud,
 };
-// 为了确保产品上公有云，升级v2的时候，默认导出的acud的主题变量
 export default acud;
