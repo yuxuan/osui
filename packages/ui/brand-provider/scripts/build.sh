8d1ab90234cc0ffa54cb113df676757af7347a43
Yuxuan Huo <yuxuan.huo2011@gmail.com>
2023-04-04 19:13:46 +0800

    去掉没有less文件的copy less指令

--- packages/ui/brand-provider/scripts/build.sh
+++ packages/ui/brand-provider/scripts/build.sh
@@ -5,9 +5,11 @@ tsc -p .
 
 swc src -d es --source-maps
 cp tmp/* es
+# cp src/*.less es
 
 swc src -d lib --source-maps --config module.type=commonjs
 cp tmp/* lib
+# cp src/*.less lib
 
 rm -rf tmp
 
