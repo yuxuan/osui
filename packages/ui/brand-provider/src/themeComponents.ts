7b0e5ff24756ccc8c5a3421e9f3633be11caf174
gongping01 <gongping01@baidu.com>
2023-04-07 13:53:52 +0800

    增加config的theme及部分组件调整

---
+++ packages/ui/brand-provider/src/themeComponents.ts
@@ -0,0 +1,39 @@
+import {ThemeConfig} from 'antd';
+
+export const components: ThemeConfig['components'] = {
+    Button: {
+        paddingContentHorizontal: 12,
+    },
+    Divider: {
+        colorSplit: 'rgba(0, 0, 0, 0.06)',
+    },
+    Input: {
+        paddingSM: 13,
+        controlPaddingHorizontal: 13,
+        colorBgContainerDisabled: '#F7F7F9',
+        colorFillAlter: '#F7F7F9',
+        colorTextPlaceholder: '#B8BABF'
+    },
+    Alert: {
+        colorInfoBorder: '#E6F0FF',
+        colorInfoBg: '#E6F0FF',
+        colorWarningBorder: '#FFF4E6',
+        colorWarningBg: '#FFF4E6',
+        colorSuccessBorder: '#ECFFE6',
+        colorSuccessBg: '#ECFFE6',
+        colorErrorBorder: '#FFE8E6',
+        colorErrorBg: '#FFE8E6',
+        lineHeight: 1.5,
+        fontSize: 12
+    },
+    Modal: {
+        paddingMD: 0,
+        paddingContentHorizontalLG: 0,
+        marginXS: 0,
+        marginSM: 0,
+        lineHeightHeading5: 1.375,
+        lineHeight:1.5,
+        fontSize: 12,
+        paddingLG: 24,
+    },
+}
