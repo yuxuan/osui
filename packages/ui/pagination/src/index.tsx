35acdff8b2135d341099de32db26ef31591ada8a
gongping01 <gongping01@baidu.com>
2023-04-19 14:50:30 +0800

    分页期 index.tsx 代码风格微调

--- packages/ui/pagination/src/index.tsx
+++ packages/ui/pagination/src/index.tsx
@@ -1,8 +1,7 @@
-import { Pagination as AntdPagination} from 'antd';
-import type { PaginationProps } from 'antd/es/Pagination';
-import React, {FC, useRef, useLayoutEffect, useEffect, useState} from 'react';
+import {Pagination as AntdPagination} from 'antd';
+import type {PaginationProps} from 'antd/es/Pagination';
+import React, {FC} from 'react';
 import './index.less';
-
 
 const icloudLocale =  { 'jump_to': '跳转至', 'page': '', jump_to_confirm: 'Go' };
 const Pagination: FC<PaginationProps> = (props) => {
@@ -19,5 +18,6 @@ const Pagination: FC<PaginationProps> = (props) => {
         </div>
     )
 };
+
 export default Pagination;
 
