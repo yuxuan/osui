723e9f971d8637fb35ed1ad237a4f4de56efbb55
nxg <nxg@nxgdeMacBook-Pro.local>
2025-10-20 14:58:55 +0800

    Switch Demo调整

--- packages/ui/switch/stories/icloud.stories.tsx
+++ packages/ui/switch/stories/icloud.stories.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {Space} from 'antd';
 import {IconCheckOutlined, IconCloseOutlined} from '@osui/icons';
 import BrandProvider from '@osui/brand-provider';
+import Divider from '@osui/divider';
 import Switch from '../src';
 
 export default {
@@ -10,6 +11,94 @@ export default {
 };
 
 export const Demo = () => {
+
+    return (
+        <BrandProvider brand="icloud">
+            <Divider>常规尺寸</Divider>
+            <Space size={50}>
+                <div style={{width: 60}}>正常</div>
+                <Space>
+                    <Switch />
+                    <Switch defaultChecked />
+                </Space>
+            </Space>
+            <br />
+            <br />
+            <Space size={50}>
+                <div style={{width: 60}}>禁用</div>
+                <Space>
+                    <Switch disabled />
+                    <Switch disabled defaultChecked />
+                </Space>
+            </Space>
+            <br />
+            <br />
+            <Space size={50}>
+                <div style={{width: 60}}>Loading</div>
+                <Space>
+                    <Switch loading />
+                    <Switch loading defaultChecked />
+                </Space>
+            </Space>
+
+            <Divider>中尺寸</Divider>
+            <Space size={50}>
+                <div style={{width: 60}}>正常</div>
+                <Space>
+                    <Switch />
+                    <Switch defaultChecked />
+                </Space>
+            </Space>
+            <br />
+            <br />
+            <Space size={50}>
+                <div style={{width: 60}}>禁用</div>
+                <Space>
+                    <Switch disabled />
+                    <Switch disabled defaultChecked />
+                </Space>
+            </Space>
+            <br />
+            <br />
+            <Space size={50}>
+                <div style={{width: 60}}>Loading</div>
+                <Space>
+                    <Switch loading />
+                    <Switch loading defaultChecked />
+                </Space>
+            </Space>
+
+            <Divider>小尺寸</Divider>
+            <Space size={50}>
+                <div style={{width: 60}}>正常</div>
+                <Space>
+                    <Switch size="small" />
+                    <Switch size="small" defaultChecked />
+                </Space>
+            </Space>
+            <br />
+            <br />
+            <Space size={50}>
+                <div style={{width: 60}}>禁用</div>
+                <Space>
+                    <Switch size="small" disabled />
+                    <Switch size="small" disabled defaultChecked />
+                </Space>
+            </Space>
+            <br />
+            <br />
+            <Space size={50}>
+                <div style={{width: 60}}>Loading</div>
+                <Space>
+                    <Switch size="small" loading />
+                    <Switch size="small" loading defaultChecked />
+                </Space>
+            </Space>
+        </BrandProvider>
+    );
+};
+
+export const OldDemo = () => {
     return (
         <BrandProvider brand="icloud">
             <h3>基础开关</h3>
