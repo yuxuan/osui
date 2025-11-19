e8d82b7468cb5bfaaa4df5357a03c5659086ed5e
nxg <nxg@nxgdeMacBook-Pro.local>
2025-11-19 13:32:15 +0800

    checkbox样式调整

--- packages/ui/brand-provider/src/themeComponents.ts
+++ packages/ui/brand-provider/src/themeComponents.ts
@@ -162,8 +162,5 @@ export const components: ThemeConfig['components'] = {
         // nodeSelectedColor: acudSemanticTokens['--text-color-brand'],
         // directoryNodeSelectedColor: acudSemanticTokens['--text-color-brand'],
     },
-    Radio: {
-        // buttonCheckedBg: acudSemanticTokens['--brand-color-light-hover'],
-    },
 };
 
