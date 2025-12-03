01c7d01cbe606c3a58d784997e09d3479aac1963
nanxg <56257398+nanxg@users.noreply.github.com>
2025-12-03 10:37:09 +0800

    V4 less nxg (#45)
    * segmented Demo更新
    
    * Breadcrumb面包屑 Demo更新
    
    * Tag Badge Demo调整
    
    * Popover Demo调整
    
    * Tooltip Demo调整
    
    * Table Demo调整
    
    * Spin Demo调整
    
    * Select Demo调整
    
    * Radio Demo调整
    
    * Checkbox Demo调整
    
    * Switch Demo调整
    
    * message Demo调整
    
    * message Demo调整
    
    * notification Demo调整
    
    * Alert Demo调整
    
    * Drawer Demo调整
    
    * Modal Demo调整
    
    * Segmented组件 样式调整
    
    * Switch组件 样式调整
    
    * alert Demo调整
    
    * alert 样式调整
    
    * tree 样式demo调整
    
    * radio 样式demo调整
    
    * checkbox样式调整
    
    * message提示 样式调整
    
    * notification 样式调整
    
    ---------
    
    Co-authored-by: nxg <nxg@nxgdeMacBook-Pro.local>

--- packages/ui/brand-provider/src/themeComponents.ts
+++ packages/ui/brand-provider/src/themeComponents.ts
@@ -239,5 +239,8 @@ export const components: ThemeConfig['components'] = {
         // nodeSelectedColor: acudSemanticTokens['--text-color-brand'],
         // directoryNodeSelectedColor: acudSemanticTokens['--text-color-brand'],
     },
+    Notification: {
+        width: 400,
+    },
 };
 
--- packages/ui/checkbox/stories/icloud.stories.tsx
+++ packages/ui/checkbox/stories/icloud.stories.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
+import React, {useState} from 'react';
 import BrandProvider from '@osui/brand-provider';
 import Divider from '@osui/divider';
+import {Flex} from 'antd';
 import Checkbox from '../src';
 
 const Group = Checkbox.Group;
@@ -17,16 +18,14 @@ export const Demo = () => {
     }
 
     const plainOptions = ['Apple', 'Pear', 'Orange'];
-    const options = [
-        {label: 'Apple', value: 'Apple'},
-        {label: 'Pear', value: 'Pear'},
-        {label: 'Orange', value: 'Orange'},
-    ];
     const optionsWithDisabled = [
         {label: 'Apple', value: 'Apple'},
         {label: 'Pear', value: 'Pear'},
-        {label: 'Orange', value: 'Orange', disabled: false},
+        {label: 'Orange', value: 'Orange', disabled: true},
     ];
+
+    const [indeterminate1, setindeterminate1] = useState(true);
+    const [indeterminate2, setindeterminate2] = useState(true);
     return (
         <BrandProvider>
             <div style={{padding: 30}}>
@@ -34,78 +33,99 @@ export const Demo = () => {
                 <h3>常规</h3>
                 <p style={{display: 'flex', gap: 80, paddingLeft: 20}}>
                     <span>未选</span>
-                    <section>
+                    <Flex gap={16}>
                         <Checkbox />
                         <Checkbox disabled />
-                    </section>
+                    </Flex>
                 </p>
                 <p style={{display: 'flex', gap: 80, paddingLeft: 20}}>
                     <span>已选</span>
-                    <section>
+                    <Flex gap={16}>
                         <Checkbox defaultChecked />
                         <Checkbox defaultChecked disabled />
-                    </section>
+                    </Flex>
                 </p>
                 <p style={{display: 'flex', gap: 80, paddingLeft: 20}}>
                     <span>半选</span>
-                    <section>
-                        <Checkbox indeterminate />
+                    <Flex gap={16}>
+                        <Checkbox indeterminate={indeterminate1} onChange={e => setindeterminate1(e.target.checked)} />
                         <Checkbox indeterminate disabled />
-                    </section>
+                    </Flex>
                 </p>
                 <h3>带文字</h3>
                 <p style={{display: 'flex', gap: 80, paddingLeft: 20}}>
                     <span>未选</span>
-                    <section>
+                    <Flex gap={16}>
                         <Checkbox>选项一</Checkbox>
                         <Checkbox disabled>选项二</Checkbox>
-                    </section>
+                    </Flex>
                 </p>
                 <p style={{display: 'flex', gap: 80, paddingLeft: 20}}>
                     <span>已选</span>
-                    <section>
+                    <Flex gap={16}>
                         <Checkbox defaultChecked>选项一</Checkbox>
                         <Checkbox defaultChecked disabled>选项二</Checkbox>
-                    </section>
+                    </Flex>
                 </p>
                 <p style={{display: 'flex', gap: 80, paddingLeft: 20}}>
                     <span>半选</span>
-                    <section>
-                        <Checkbox indeterminate>选项一</Checkbox>
+                    <Flex gap={16}>
+                        <Checkbox indeterminate={indeterminate2} onChange={e => setindeterminate2(e.target.checked)}>
+                            选项一
+                        </Checkbox>
                         <Checkbox indeterminate disabled>选项二</Checkbox>
-                    </section>
+                    </Flex>
                 </p>
 
                 <Divider>多选组合</Divider>
+                <p>横向</p>
                 <Group options={plainOptions} defaultValue={['Apple']} onChange={onChange} />
-                <br />
-                <br />
-                <Group options={options} defaultValue={['Pear']} onChange={onChange} />
-                <br />
-                <br />
+                <p />
                 <Group
                     options={optionsWithDisabled}
                     disabled
                     defaultValue={['Apple']}
                     onChange={onChange}
                 />
+                <p />
+                <p>竖向</p>
+                <Group
+                    options={optionsWithDisabled}
+                    defaultValue={['Apple']}
+                    onChange={onChange}
+                    style={{flexDirection: 'column'}}
+                />
 
                 <Divider>卡片多选</Divider>
                 <p>默认</p>
-                <p style={{display: 'flex', gap: 80, paddingLeft: 20}}>
-                    <span>未选</span>
-                    <section>
-                        <Checkbox />
-                        <Checkbox disabled />
-                    </section>
-                </p>
-                <p style={{display: 'flex', gap: 80, paddingLeft: 20}}>
-                    <span>已选</span>
-                    <section>
-                        <Checkbox defaultChecked />
-                        <Checkbox defaultChecked disabled />
-                    </section>
-                </p>
+                <Group defaultValue={['a']}>
+                    <Checkbox.Card
+                        value={'a'}
+                        title="自动规则打分"
+                        description="使用百度智能云 IAM 进行身份管理和访问权限控制，不兼容 OpenAI SDK"
+                    />
+                    <Checkbox.Card
+                        value={'b'}
+                        title="自动规则打分"
+                        description="使用百度智能云 IAM 进行身份管理和访问权限控制，不兼容 OpenAI SDK"
+                    />
+                </Group>
+                <p></p>
+                <p>禁用</p>
+                <Group defaultValue={['a']}>
+                    <Checkbox.Card
+                        value={'a'}
+                        title="自动规则打分"
+                        description="使用百度智能云 IAM 进行身份管理和访问权限控制，不兼容 OpenAI SDK"
+                        disabled
+                    />
+                    <Checkbox.Card
+                        value={'b'}
+                        title="自动规则打分"
+                        description="使用百度智能云 IAM 进行身份管理和访问权限控制，不兼容 OpenAI SDK"
+                        disabled
+                    />
+                </Group>
             </div>
         </BrandProvider>
     );
